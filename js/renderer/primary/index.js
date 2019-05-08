36999590066913ce7501728afe5c351cc796c8ea
dehmer <horst.dehmer@gmail.com>
2019-05-08 10:57:21 +0200

    fresh start: react, webpack, material-ui, electron-builder.

--- js/renderer/primary/index.js
+++
@@ -1,4 +0,0 @@
-require('./js/renderer/primary/map') // singleton Leaflet map instance.
-require('./js/renderer/primary/osd')
-require('./js/renderer/primary/osd-dtg').init()
-require('./js/renderer/common/datetime')
