9c20ac2bc1cef405f9e78dbc562d9ac2651b5212
dehmer <horst.dehmer@gmail.com>
2019-05-09 19:00:02 +0200

    material-ui: added dependencies.

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
 import { K } from '../shared/combinators'
+import 'typeface-roboto' // material-ui dependency
 
 const rootId = 'root'
 
