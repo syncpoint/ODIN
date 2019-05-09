422db1c6fa9251980e622f39a8d987e032d9938d
dehmer <horst.dehmer@gmail.com>
2019-05-09 17:42:11 +0200

    webpack: generating index.html.

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
+import { K } from '../shared/combinators'
+
+const rootId = 'root'
+
+// Create root </div> to mount application in:
+document.body.appendChild(K(document.createElement('div'))(div => {
+  div.id = rootId
+}))
 
 ReactDOM.render(
   <App></App>,
-  document.getElementById('root')
+  document.getElementById(rootId)
 )
