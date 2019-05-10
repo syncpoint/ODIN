da9b687f68f41c615a7090a54d149c656415af38
dehmer <horst.dehmer@gmail.com>
2019-05-10 15:19:38 +0200

    CSS/layout; OSD (wip).

--- src/renderer/components/App.js
+++ src/renderer/components/App.js
@@ -1,5 +1,30 @@
 import React from 'react'
 import Map from './Map'
+import { withStyles } from '@material-ui/core/styles'
+import OSD from './OSD'
+import { currentDateTime } from '../../shared/datetime'
+
+const styles = {
+  overlay: {
+    position: 'fixed',
+    top: '1em',
+    left: '1em',
+    bottom: '1.5em',
+    right: '1em',
+    zIndex: 20,
+    display: 'grid',
+    gridTemplateColumns: 'auto',
+    gridTemplateRows: '5em auto',
+    gridGap: '1em',
+    pointerEvents: 'none'
+  },
+
+  contentPanel: {
+    background: 'rgba(100, 100, 0, 0.4)',
+    gridRowStart: 2,
+    gridColumnStart: 1
+  }
+}
 
 const center = L.latLng(48.65400545105681, 15.319061279296877)
 const mapOptions = {
@@ -16,14 +41,41 @@ const tileProvider = {
   "attribution": "&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors"
 }
 
-const App = props =>
-  <div>
-    <Map
-      id='map'
-      className='map'
-      tileProvider={ tileProvider }
-      options={ mapOptions }
-    />
-  </div>
+class App extends React.Component {
+  constructor(props) {
+    super(props)
 
-export default App
+    this.state = {
+      osd: {
+        C1: currentDateTime()
+      }
+    }
+  }
+
+  componentDidMount(prevProps, prevState) {
+    const osd = { ...this.state.osd, C1: currentDateTime() }
+    this.clockInterval = setInterval(() => {
+      this.setState({ ...this.state, osd })
+    }, 1000)
+  }
+
+
+  render() {
+    return (
+      <div>
+        <Map
+          id='map'
+          className='map'
+          tileProvider={ tileProvider }
+          options={ mapOptions }
+        />
+        <div className={ this.props.classes.overlay }>
+          <OSD osd={ this.state.osd }/>
+          <div className={ this.props.classes.contentPanel }></div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default withStyles(styles)(App)
