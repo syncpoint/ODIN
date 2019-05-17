273df2776d3cc536b7b6f01ea7fa439060849d95
dehmer <horst.dehmer@gmail.com>
2019-05-17 09:54:30 +0200

    map: extracted command handlers.

--- src/renderer/components/Map.js
+++ src/renderer/components/Map.js
@@ -5,12 +5,12 @@ import PropTypes from 'prop-types'
 import { ipcRenderer, clipboard } from 'electron'
 import EventEmitter from 'events'
 import settings from 'electron-settings'
+import 'leaflet/dist/leaflet.css'
 import path from 'path'
 import * as R from 'ramda'
 import { K, noop } from '../../shared/combinators'
 import Timed from '../../shared/timed'
 import Disposable from '../../shared/disposable'
-import 'leaflet/dist/leaflet.css'
 import Leaflet from '../leaflet'
 
 // https://github.com/PaulLeCam/react-leaflet/issues/255
@@ -55,6 +55,97 @@ const descriptors = {
 const defaultValues = () => Object.entries(descriptors)
   .reduce((acc, [name, { value, unit }]) => K(acc)(acc => (acc[name] = { value, unit })), {})
 
+const commandHandlers = eventBus => ({
+
+  COMMAND_MAP_TILE_PROVIDER: function (options) {
+    Leaflet.layers(this.map)
+      .filter(layer => layer instanceof L.TileLayer)
+      .forEach(layer => this.map.removeLayer(layer))
+    L.tileLayer(options.url, options).addTo(this.map)
+    settings.set('tileProvider', options)
+  },
+
+  COMMAND_COPY_COORDS: function () {
+    const container = document.getElementById('map')
+    const originalCursor = container.style.cursor
+
+    const onClick = event => {
+      container.style.cursor = originalCursor
+      container.removeEventListener('click', onClick)
+
+      const pointXY = L.point(event.layerX, event.layerY)
+      const latlng = this.map.layerPointToLatLng(pointXY).wrap()
+
+      // TODO: get coordinate format from user setting (once implemented)
+      clipboard.writeText(`${latlng.lat} ${latlng.lng}`)
+      const originalFilter = container.style.filter
+      const reset = () => (container.style.filter = originalFilter)
+      container.style.filter = 'invert(100%)'
+      setTimeout(reset, 50)
+      eventBus.emit('OSD_MESSAGE', { message: `Coordinates Copied`, duration: 1500 })
+    }
+
+    if (container.style.cursor === '') {
+      container.style.cursor = 'crosshair'
+      container.addEventListener('click', onClick)
+    }
+  },
+
+  COMMAND_RESET_FILTERS: function () {
+    const values = defaultValues()
+    settings.set('displayFilters', values)
+    this.updateDisplayFilters(values)
+  },
+
+  COMMAND_ADJUST: function (filter) {
+    if (this.filterControl) this.filterControl.dispose()
+
+    this.filterControl = (() => {
+      const descriptor = descriptors[filter]
+      const currentValues = settings.get('displayFilters') || defaultValues()
+      const apply = () => settings.set('displayFilters', currentValues)
+      const cancel = () => this.updateDisplayFilters(settings.get('displayFilters') || defaultValues())
+      const disposable = Disposable.of({})
+      const timer = Timed.of(3000, R.compose(disposable.dispose, apply))({})
+
+      eventBus.emit('OSD_MESSAGE', { message: `${descriptor.label}: ${currentValues[filter].value}${descriptor.unit}` })
+
+      const refresh = value => {
+        if (value < descriptor.min || value > descriptor.max) return
+        currentValues[filter].value = value
+        eventBus.emit('OSD_MESSAGE', { message: `${descriptor.label}: ${value}${descriptor.unit}` })
+        timer.refreshTimeout(2000)
+        this.updateDisplayFilters(currentValues)
+      }
+
+      const decrease = () => refresh(currentValues[filter].value - descriptor.delta)
+      const increase = () => refresh(currentValues[filter].value + descriptor.delta)
+      const stopPropagation = event => K(event)(event => event.stopPropagation())
+
+      const actions = {
+        'ArrowLeft': R.compose(decrease, stopPropagation),
+        'ArrowDown': R.compose(decrease, stopPropagation),
+        'ArrowRight': R.compose(increase, stopPropagation),
+        'ArrowUp': R.compose(increase, stopPropagation),
+        'Escape': R.compose(disposable.dispose, cancel, stopPropagation),
+        'Enter': R.compose(disposable.dispose, apply, stopPropagation)
+      }
+
+      // NOTE: To prevent panning, we capture keydown events so that they don't reach the map (while trickling down).
+      const onkeydown = event => (actions[event.key] || noop)(event)
+      const useCapture = true
+      document.addEventListener('keydown', onkeydown, useCapture)
+
+      disposable.addDisposable(timer.clearTimeout)
+      disposable.addDisposable(() => document.removeEventListener('keydown', onkeydown, useCapture))
+      disposable.addDisposable(() => eventBus.emit('OSD_MESSAGE', { message: '' }))
+      disposable.addDisposable(() => this.map._container.focus())
+
+      return disposable
+    })()
+  }
+})
+
 class Map extends React.Component {
   updateDisplayFilters (filterValues) {
     const styles = Leaflet
@@ -69,11 +160,10 @@ class Map extends React.Component {
   }
 
   componentDidMount () {
-    const { id, options } = this.props
+    const { id, options, eventBus } = this.props
     const tileProvider = settings.get('tileProvider') || defautTileProvider
     const displayFilters = settings.get('displayFilters') || defaultValues()
     const viewPort = settings.get('viewPort')
-    const { eventBus } = this.props
 
     // Override center/zoom if available from settings:
     if (viewPort) {
@@ -89,93 +179,8 @@ class Map extends React.Component {
 
     this.map.on('click', () => this.props.onClick())
 
-    ipcRenderer.on('COMMAND_MAP_TILE_PROVIDER', (_, options) => {
-      Leaflet.layers(this.map)
-        .filter(layer => layer instanceof L.TileLayer)
-        .forEach(layer => this.map.removeLayer(layer))
-      L.tileLayer(options.url, options).addTo(this.map)
-      settings.set('tileProvider', options)
-    })
-
-    ipcRenderer.on('COMMAND_COPY_COORDS', (_, args) => {
-      const container = document.getElementById('map')
-      const originalCursor = container.style.cursor
-
-      const onClick = event => {
-        container.style.cursor = originalCursor
-        container.removeEventListener('click', onClick)
-
-        const pointXY = L.point(event.layerX, event.layerY)
-        const latlng = this.map.layerPointToLatLng(pointXY).wrap()
-
-        // TODO: get coordinate format from user setting (once implemented)
-        clipboard.writeText(`${latlng.lat} ${latlng.lng}`)
-        const originalFilter = container.style.filter
-        const reset = () => (container.style.filter = originalFilter)
-        container.style.filter = 'invert(100%)'
-        setTimeout(reset, 50)
-        eventBus.emit('OSD_MESSAGE', { message: `Coordinates Copied`, duration: 1500 })
-      }
-
-      if (container.style.cursor === '') {
-        container.style.cursor = 'crosshair'
-        container.addEventListener('click', onClick)
-      }
-    })
-
-    ipcRenderer.on('COMMAND_RESET_FILTERS', (_, args) => {
-      const values = defaultValues()
-      settings.set('displayFilters', values)
-      this.updateDisplayFilters(values)
-    })
-
-    ipcRenderer.on('COMMAND_ADJUST', (_, filter) => {
-      const { eventBus } = this.props
-      if (this.filterControl) this.filterControl.dispose()
-
-      this.filterControl = (() => {
-        const descriptor = descriptors[filter]
-        const currentValues = settings.get('displayFilters') || defaultValues()
-        const apply = () => settings.set('displayFilters', currentValues)
-        const cancel = () => this.updateDisplayFilters(settings.get('displayFilters') || defaultValues())
-        const disposable = Disposable.of({})
-        const timer = Timed.of(3000, R.compose(disposable.dispose, apply))({})
-
-        eventBus.emit('OSD_MESSAGE', { message: `${descriptor.label}: ${currentValues[filter].value}${descriptor.unit}` })
-
-        const refresh = value => {
-          if (value < descriptor.min || value > descriptor.max) return
-          currentValues[filter].value = value
-          eventBus.emit('OSD_MESSAGE', { message: `${descriptor.label}: ${value}${descriptor.unit}` })
-          timer.refreshTimeout(2000)
-          this.updateDisplayFilters(currentValues)
-        }
-
-        const decrease = () => refresh(currentValues[filter].value - descriptor.delta)
-        const increase = () => refresh(currentValues[filter].value + descriptor.delta)
-        const stopPropagation = event => K(event)(event => event.stopPropagation())
-
-        const actions = {
-          'ArrowLeft': R.compose(decrease, stopPropagation),
-          'ArrowDown': R.compose(decrease, stopPropagation),
-          'ArrowRight': R.compose(increase, stopPropagation),
-          'ArrowUp': R.compose(increase, stopPropagation),
-          'Escape': R.compose(disposable.dispose, cancel, stopPropagation),
-          'Enter': R.compose(disposable.dispose, apply, stopPropagation)
-        }
-
-        // NOTE: To prevent panning, we capture keydown events so that they don't reach the map (while trickling down).
-        const onkeydown = event => (actions[event.key] || noop)(event)
-        const useCapture = true
-        document.addEventListener('keydown', onkeydown, useCapture)
-
-        disposable.addDisposable(timer.clearTimeout)
-        disposable.addDisposable(() => document.removeEventListener('keydown', onkeydown, useCapture))
-        disposable.addDisposable(() => eventBus.emit('OSD_MESSAGE', { message: '' }))
-        disposable.addDisposable(() => this.map._container.focus())
-
-        return disposable
-      })()
+    Object.entries(commandHandlers(eventBus)).map(([channel, handler]) => {
+      ipcRenderer.on(channel, (_, args) => handler.bind(this)(args))
     })
 
     this.map.on('moveend', () => {
