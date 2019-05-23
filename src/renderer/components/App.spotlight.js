6b08f3f43b37bee9ae261ff5d1feae69b34654a2
dehmer <horst.dehmer@gmail.com>
2019-05-23 19:39:09 +0200

    Spotlight: sorting booksmarks.

--- src/renderer/components/App.spotlight.js
+++ src/renderer/components/App.spotlight.js
@@ -33,6 +33,7 @@ const bookmarks = options => term => {
   const { context } = options
   const items = Object.entries((mapSettings.get('bookmarks') || {}))
     .filter(([id, _]) => id.search(new RegExp(term, 'i')) !== -1)
+    .sort((a, b) => a[0].localeCompare(b[0]))
     .map(([id, bookmark]) => ({
       key: id,
       text: <ListItemText primary={ id } secondary={ 'Bookmark' }/>,
