b3e679b02abeb6e650bc30d5a326a1b671a76c2f
dehmer <horst.dehmer@gmail.com>
2019-05-26 14:23:19 +0200

    #46: simplified marker/icon switching.

--- src/renderer/components/map/poi-layer.js
+++ src/renderer/components/map/poi-layer.js
@@ -15,27 +15,27 @@ const poiLayer = map => {
   let layer = null
   let selection
   const zoom = map.getZoom()
+
+  // POI/id -> marker (layer)
   const featureLayers = {}
 
   const deselect = () => {
     if (!selection) return
-    layer.removeLayer(featureLayers[selection].highlight)
-    layer.addLayer(featureLayers[selection].standard)
+    const layer = featureLayers[selection]
+    layer.setIcon(layer.options.icons.standard)
     selection = undefined
   }
 
   const select = id => {
     deselect()
-    layer.removeLayer(featureLayers[id].standard)
-    layer.addLayer(featureLayers[id].highlight)
     selection = id
+    const layer = featureLayers[selection]
+    layer.setIcon(layer.options.icons.highlighted)
   }
 
   map.on('click', deselect)
-
   map.on('keydown', event => {
     if (!selection) return
-
     const { originalEvent } = event
     if (originalEvent.key === 'Backspace' && originalEvent.metaKey) poiStore.remove(selection)
     else if (originalEvent.key === 'Delete') poiStore.remove(selection)
@@ -44,67 +44,53 @@ const poiLayer = map => {
   const pointToLayer = (feature, latlng) => {
     const { id, sidc } = feature.properties
     const size = sizes[zoom] || 40
-    const options = {
-      size
-    }
-
+    const options = { size }
     if (zoom > 12) options.uniqueDesignation = id
-
     const symbol = options => new ms.Symbol(sidc, options)
 
-    const icon = symbol => L.divIcon({
+    const divIcon = symbol => L.divIcon({
       className: '',
       html: symbol.asSVG(),
       iconAnchor: new L.Point(symbol.getAnchor().x, symbol.getAnchor().y)
     })
 
     // Create marker layer:
-    const marker = icon => {
-      const marker = L.marker(latlng, {
-        icon,
-        keyboard: false, // default: true
-        draggable: true, // default: false
-        autoPan: true,
-        autoPanSpeed: 10 // default 10
-      })
+    const marker = L.marker(latlng, {
+      id,
+      icons: {
+        standard: divIcon(symbol(options)),
+        highlighted: divIcon(symbol({
+          ...options,
+          monoColor: 'white',
+          outlineColor: 'grey',
+          outlineWidth: 4
+        }))
+      },
+      keyboard: false, // default: true
+      draggable: true, // default: false
+      autoPan: true,
+      autoPanSpeed: 10 // default: 10
+    })
 
-      marker.on('moveend', event => {
-        const { target } = event
-        const { lat, lng } = target.getLatLng()
-        poiStore.move(id, { lat, lng })
+    marker.on('moveend', event => {
+      const { target } = event
+      const id = target.options.id
+      const { lat, lng } = target.getLatLng()
+      poiStore.move(id, { lat, lng })
+      select(id)
+    })
 
-        // synchronize positions for standard and highlighted markers:
-        if (featureLayers[id].standard === target) {
-          featureLayers[id].highlight.setLatLng(featureLayers[id].standard.getLatLng())
-        } else {
-          featureLayers[id].standard.setLatLng(featureLayers[id].highlight.getLatLng())
-        }
+    marker.on('click', event => {
+      const id = event.target.options.id
+      if (selection === id) return deselect(id)
+      else select(id)
+    })
 
-        select(id)
-      })
+    marker.id = id
 
-      marker.on('click', event => {
-        const { target } = event
-        if (!featureLayers[id]) return
-        if (featureLayers[id].standard === target) select(id)
-        else deselect()
-      })
-
-      marker.id = id
-      return marker
-    }
-
-    featureLayers[feature.properties.id] = {
-      standard: marker(icon(symbol(options))),
-      highlight: marker(icon(symbol({
-        ...options,
-        monoColor: 'white',
-        outlineColor: 'grey',
-        outlineWidth: 4
-      })))
-    }
-
-    return featureLayers[feature.properties.id].standard
+    marker.setIcon(marker.options.icons.standard)
+    featureLayers[feature.properties.id] = marker
+    return featureLayers[feature.properties.id]
   }
 
   const feature = poi => ({
@@ -132,8 +118,7 @@ const poiLayer = map => {
 
   poiStore.evented.on('removed', id => {
     if (featureLayers[id]) {
-      layer.removeLayer(featureLayers[id].standard)
-      layer.removeLayer(featureLayers[id].highlight)
+      layer.removeLayer(featureLayers[id])
       delete featureLayers[id]
       if (selection === id) selection = undefined
     }
