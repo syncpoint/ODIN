638ee1a9232a3b104200dfe9f2610e0e15912d84
dehmer <horst.dehmer@gmail.com>
2019-05-28 19:08:51 +0200

    added view option: Retina/HiDPI support.

--- src/renderer/components/map/tile-provider.js
+++ src/renderer/components/map/tile-provider.js
@@ -2,10 +2,34 @@ import L from 'leaflet'
 import Leaflet from '../../leaflet'
 import mapSettings from './settings'
 
-export const COMMAND_MAP_TILE_PROVIDER = ({ map }) => options => {
+export const tileProvider = () => {
+  const tileProvider = mapSettings.get('tileProvider') || defautTileProvider
+  tileProvider.detectRetina = mapSettings.get('hiDPISupport') || false
+  return tileProvider
+}
+
+const updateTileLayer = map => {
   Leaflet.layers(map)
     .filter(layer => layer instanceof L.TileLayer)
     .forEach(layer => map.removeLayer(layer))
-  L.tileLayer(options.url, options).addTo(map)
+  console.log(tileProvider())
+  L.tileLayer(tileProvider().url, tileProvider()).addTo(map)
+}
+
+const defautTileProvider = {
+  id: 'OpenStreetMap.Mapnik',
+  name: 'OpenStreetMap',
+  url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  maxZoom: 19,
+  attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors`
+}
+
+export const COMMAND_MAP_TILE_PROVIDER = ({ map }) => options => {
   mapSettings.set('tileProvider', options)
+  updateTileLayer(map)
+}
+
+export const COMMAND_HIDPI_SUPPORT = ({ map }) => () => {
+  // NOTE: hiDPISupport aleady updated in map settings (main/menu).
+  updateTileLayer(map)
 }
