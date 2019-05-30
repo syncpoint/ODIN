de0b1a0df02b567d5130dda124aa27b15dd887d1
dehmer <horst.dehmer@gmail.com>
2019-05-30 08:54:53 +0200

    POI: preliminary clipboard support.

--- src/renderer/components/App.clipboard.js
+++ src/renderer/components/App.clipboard.js
@@ -1,11 +1,47 @@
 import Mousetrap from 'mousetrap'
-import selection from './App.selection'
+import { clipboard } from 'electron'
+
+const handlers = []
+
+const registerHandler = handler => {
+  handlers.push(handler)
+}
 
 Mousetrap.bind(['mod+c'], () => {
-  console.log(selection.selected())
+  for (let index in handlers) {
+    const handler = handlers[index]
+    if (!handler.copy) continue
+    const text = handler.copy()
+    if (!text) continue
+    clipboard.writeText(text)
+  }
 })
 
-Mousetrap.bind(['mod+x'], event => console.log('CUT', event))
-Mousetrap.bind(['mod+v'], event => console.log('PASTE', event))
-Mousetrap.bind(['mod+z'], event => console.log('UNDO', event))
-Mousetrap.bind(['mod+shift+z'], event => console.log('REDO', event))
+Mousetrap.bind(['mod+x'], () => {
+  for (let index in handlers) {
+    const handler = handlers[index]
+    if (!handler.cut) continue
+    const text = handler.cut()
+    if (!text) continue
+    clipboard.writeText(text)
+  }
+})
+
+Mousetrap.bind(['mod+v'], () => {
+  const text = clipboard.readText()
+  if (!text) return
+
+  for (let index in handlers) {
+    const handler = handlers[index]
+    if (!handler.paste) continue
+    handler.paste(text)
+  }
+})
+
+Mousetrap.bind(['mod+z'], () => handlers.forEach(handler => handler.undo()))
+Mousetrap.bind(['mod+shift+z'], () => handlers.forEach(handler => handler.redo()))
+
+
+export default {
+  registerHandler
+}
