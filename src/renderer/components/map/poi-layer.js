ed57145c92d82fc5c684f4b731909bd265ecfbf0
dehmer <horst.dehmer@gmail.com>
2019-06-02 07:33:53 +0200

    clean-up: minor refactorings.

--- src/renderer/components/map/poi-layer.js
+++ src/renderer/components/map/poi-layer.js
@@ -41,28 +41,53 @@ const feature = poi => ({
   properties: { name: poi.name, uuid: poi.uuid, sidc: 'GFGPGPRI----' }
 })
 
+// ==> marker symbol and icons
+
+const symbol = (sidc, options) => new ms.Symbol(sidc, options)
+
+const icon = symbol => L.divIcon({
+  className: '',
+  html: symbol.asSVG(),
+  iconAnchor: new L.Point(symbol.getAnchor().x, symbol.getAnchor().y)
+})
+
+const standardIcon = (sidc, options) => icon(symbol(sidc, options))
+
+const highlightedIcon = (sidc, options) => icon(symbol(sidc, {
+  ...options,
+  monoColor: 'white',
+  outlineColor: 'grey',
+  outlineWidth: 4
+}))
+
+// <== marker symbol and icons
+
+// ==> marker mouse handlers
+
+const moveend = ({ target }) => {
+  const { uuid } = target.options
+  const { lat, lng } = target.getLatLng()
+  store.move(uuid, { lat, lng })
+  select(uuid)
+}
+
+const click = ({ target }) => {
+  const { uuid } = target.options
+  select(uuid)
+}
+
+// <== marker mouse handlers
+
 // pointToLayer: feature -> latlng -> layer
 const pointToLayer = (feature, latlng) => {
   const { uuid, name, sidc } = feature.properties
-  const size = 34
-  const symbol = options => new ms.Symbol(sidc, options)
-  const icon = symbol => L.divIcon({
-    className: '',
-    html: symbol.asSVG(),
-    iconAnchor: new L.Point(symbol.getAnchor().x, symbol.getAnchor().y)
-  })
 
-  const options = { size, uniqueDesignation: name }
+  const options = { size: 34, uniqueDesignation: name }
   const marker = L.marker(latlng, {
     uuid,
     icons: {
-      standard: icon(symbol(options)),
-      highlighted: icon(symbol({
-        ...options,
-        monoColor: 'white',
-        outlineColor: 'grey',
-        outlineWidth: 4
-      }))
+      standard: standardIcon(sidc, options),
+      highlighted: highlightedIcon(sidc, options)
     },
     keyboard: false, // default: true
     draggable: true, // default: false
@@ -70,98 +95,94 @@ const pointToLayer = (feature, latlng) => {
     autoPanSpeed: 10 // default: 10
   })
 
-  marker.on('moveend', ({ target }) => {
-    const { uuid } = target.options
-    const { lat, lng } = target.getLatLng()
-    store.move(uuid, { lat, lng })
-    select(uuid)
-  })
+  marker.on('moveend', moveend)
+  marker.on('click', click)
 
-  marker.on('click', event => {
-    const { target } = event
-    const { uuid } = target.options
-    select(uuid)
-  })
-
+  // Set icon depending on current selection:
   const icons = marker.options.icons
   marker.setIcon(selected() === uuid ? icons.highlighted : icons.standard)
   featureLayers[uuid] = marker
   return marker
 }
 
-const poiLayer = map => {
-
-  const add = poi => {
-    layer.addData(feature(poi))
-  }
+const layerOps = layer => {
+  const add = poi => layer.addData(feature(poi))
 
   const remove = ({ uuid }) => {
     if (uuid && featureLayers[uuid]) {
       deselect()
       layer.removeLayer(featureLayers[uuid])
       delete featureLayers[uuid]
-
-      // FIXME: cyclic call when triggered from store event 'removed':
-      store.remove(uuid)
+      store.remove(uuid) // FIXME: cyclic call when triggered from store event 'removed':
     }
   }
 
-  const removeSelected = () => remove({ uuid: selected() })
-
-  const renamed = ({ uuid }) => {
+  const rename = ({ uuid }) => {
     layer.removeLayer(featureLayers[uuid])
     delete featureLayers[uuid]
     add({ uuid, ...store.state()[uuid] })
   }
 
+  return {
+    add,
+    remove,
+    rename
+  }
+}
 
-  map.on('click', () => deselect())
-  map.on('keydown', event => {
-    const { originalEvent } = event
-    if (originalEvent.key === 'Backspace' && originalEvent.metaKey) removeSelected()
-    else if (originalEvent.key === 'Delete') removeSelected()
-    else if (originalEvent.key === 'Escape') deselect()
-  })
+const clipboardHandlers = {
+  copy: () => {
+    const selected = selection.selected()
+    if (!selected) return
+    const poi = store.state()[selected.uuid]
+    return JSON.stringify(poi)
+  },
 
+  cut: () => {
+    const selected = selection.selected()
+    if (selected) {
+      const poi = store.state()[selected.uuid]
+      store.remove(selected.uuid)
+      return JSON.stringify(poi)
+    }
+  },
+
+  paste: text => {
+    // TODO: Check if clipboard content is of expected type.
+    // TODO: Disambiguate name.
+    const poi = JSON.parse(text)
+    store.add(uuid(), poi)
+  }
+}
+
+const poiLayer = map => {
+
+  // Add empty layer to map:
   const features = { type: 'FeatureCollection', features: [] }
   const layer = L.geoJSON(features, { pointToLayer })
   layer.id = 'POI_LAYER'
   layer.addTo(map)
 
+  const markers = layerOps(layer)
 
-  store.on('added', add)
-  store.on('removed', remove)
-  store.on('renamed', renamed)
+  map.on('click', () => deselect())
+  map.on('keydown', event => {
+    const { originalEvent } = event
+    const removeSelected = () => markers.remove({ uuid: selected() })
+    if (originalEvent.key === 'Backspace' && originalEvent.metaKey) removeSelected()
+    else if (originalEvent.key === 'Delete') removeSelected()
+    else if (originalEvent.key === 'Escape') deselect()
+  })
 
-  store.once('ready', model => Object.entries(model)
+  store.on('added', markers.add)
+  store.on('removed', markers.remove)
+  store.on('renamed', markers.rename)
+  store.once('ready', state => Object.entries(state)
     .map(([uuid, poi]) => ({ uuid, ...poi }))
-    .forEach(add)
+    .forEach(markers.add)
   )
 
-  clipboard.registerHandler({
-    copy: () => {
-      const selected = selection.selected()
-      if (!selected) return
-      const poi = store.state()[selected.uuid]
-      return JSON.stringify(poi)
-    },
-
-    cut: () => {
-      const selected = selection.selected()
-      if (selected) {
-        const poi = store.state()[selected.uuid]
-        store.remove(selected.uuid)
-        return JSON.stringify(poi)
-      }
-    },
-
-    paste: text => {
-      // TODO: Check if clipboard content is of expected type.
-      // TODO: Disambiguate name.
-      const poi = JSON.parse(text)
-      store.add(uuid(), poi)
-    }
-  })
+  clipboard.registerHandler(clipboardHandlers)
 }
 
 export default poiLayer
