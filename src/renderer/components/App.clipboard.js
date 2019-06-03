af568eaf0245338819715853634f1101358fb277
dehmer <horst.dehmer@syncpoint.io>
2019-06-03 16:02:23 +0200

    clipboard: moved copy/cut handling.

--- src/renderer/components/App.clipboard.js
+++ src/renderer/components/App.clipboard.js
@@ -7,26 +7,6 @@ const registerHandler = handler => {
   handlers.push(handler)
 }
 
-Mousetrap.bind(['mod+c'], () => {
-  for (let index in handlers) {
-    const handler = handlers[index]
-    if (!handler.copy) continue
-    const text = handler.copy()
-    if (!text) continue
-    clipboard.writeText(text)
-  }
-})
-
-Mousetrap.bind(['mod+x'], () => {
-  for (let index in handlers) {
-    const handler = handlers[index]
-    if (!handler.cut) continue
-    const text = handler.cut()
-    if (!text) continue
-    clipboard.writeText(text)
-  }
-})
-
 Mousetrap.bind(['mod+v'], () => {
   const text = clipboard.readText()
   if (!text) return
@@ -40,7 +20,6 @@ Mousetrap.bind(['mod+v'], () => {
 
 Mousetrap.bind(['mod+z'], () => handlers.forEach(handler => handler.undo()))
 Mousetrap.bind(['mod+shift+z'], () => handlers.forEach(handler => handler.redo()))
-
 
 export default {
   registerHandler
