5536822286f67cf0a4f323c66718637b06635e2b
dehmer <horst.dehmer@gmail.com>
2019-06-05 06:43:29 +0200

    main: refactored menu.

---
+++ src/main/menu/menu.js
@@ -0,0 +1,22 @@
+import { Menu } from 'electron'
+import windowMenu from './window-menu'
+import editMenu from './edit-menu'
+import helpMenu from './help-menu'
+import goMenu from './go-menu'
+import viewMenu from './view-menu'
+import applicationMenu from './application-menu'
+
+const template = [
+  // darwin only (must be filtered for other platforms)
+  applicationMenu,
+
+  editMenu,
+  viewMenu,
+  goMenu,
+  windowMenu,
+  helpMenu
+]
+
+export const buildFromTemplate = () =>
+  // filter null entries (darwin only):
+  Menu.buildFromTemplate(template.filter(x => x))
