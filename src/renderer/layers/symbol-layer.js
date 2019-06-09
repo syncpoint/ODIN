d4a11f0532fd5adf2c049a4228d5584449f0efa2
dehmer <horst.dehmer@gmail.com>
2019-06-09 11:22:20 +0200

    #75: GeoJSON/fit (bbox) bounds.

--- src/renderer/layers/symbol-layer.js
+++ src/renderer/layers/symbol-layer.js
@@ -10,8 +10,39 @@ const layer = ([name, features]) => new L.GeoJSON.Symbols({
   features: () => features
 }).addTo(map)
 
+const bounds = json => {
+  if (!json.bbox) return
+  if (typeof json.bbox !== 'string') return
+
+  const bounds = (lat1, lng1, lat2, lng2) => L.latLngBounds(L.latLng(lat1, lng1), L.latLng(lat2, lng2))
+
+  // Format: PostGIS (ST_Extent())
+  const regex = /BOX\((.*)[ ]+(.*)[, ]+(.*)[ ]+(.*)\)/
+  const match = json.bbox.match(regex)
+  if (match) {
+    /* eslint-disable no-unused-vars */
+    const [_, lng1, lat1, lng2, lat2] = match
+    return bounds(lat1, lng1, lat2, lng2)
+    /* eslint-enable no-unused-vars */
+  }
+
+  // Format: GeoJSON (simple array)
+  const [lng1, lat1, lng2, lat2] = JSON.parse(json.bbox)
+  return bounds(lat1, lng1, lat2, lng2)
+}
+
+const fitBounds = bounds => {
+  if (!bounds) return
+  map.fitBounds(bounds, { animate: true })
+}
+
+
 evented.on('MAP_CREATED', reference => {
   map = reference
-  store.on('added', ({ name, file }) => layer([name, file]))
+  store.on('added', ({ name, file }) => {
+    layer([name, file])
+    fitBounds(bounds(file))
+  })
+
   store.on('ready', state => Object.entries(state).forEach(layer))
 })
