d56a692e573c6a734780bf506c4f9715129360b9
dehmer <horst.dehmer@gmail.com>
2019-06-15 15:04:10 +0200

    #77: avatar for POINT features.

--- src/renderer/model/spotlight-features.js
+++ src/renderer/model/spotlight-features.js
@@ -1,8 +1,9 @@
 import EventEmitter from 'events'
 import React from 'react'
-import { ListItemText } from '@material-ui/core'
+import { ListItemText, ListItemAvatar, Avatar } from '@material-ui/core'
 import uuid from 'uuid-random'
 import L from 'leaflet'
+import ms from 'milsymbol'
 import layerStore from '../stores/layer-store'
 import evented from '../evented'
 
@@ -24,10 +25,17 @@ export default register => {
             if (feature.geometry.type !== 'Point') return false
             return feature.id.search(new RegExp(term(), 'i')) !== -1
           })
+          .sort((a, b) => a.id.localeCompare(b.id))
           .map(feature => {
             const [lng, lat] = feature.geometry.coordinates
+            const url = new ms.Symbol(feature.properties.sidc).asCanvas().toDataURL()
             return {
               key: uuid(),
+              avatar: (
+                <ListItemAvatar>
+                  <Avatar src={ url } style={{ borderRadius: 0, width: '15%', height: '15%' }} />
+                </ListItemAvatar>
+              ),
               text: <ListItemText primary={ feature.id } secondary={ 'Feature' }/>,
               action: () => evented.emit('map.center', L.latLng(lat, lng))
             }
