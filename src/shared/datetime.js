08732fa00b535d8132e62a07ecaba76e3d98733f
dehmer <horst.dehmer@gmail.com>
2019-06-19 07:08:09 +0200

    GeoJSON/feature: display title/name and relative DTG on mouse over.

--- src/shared/datetime.js
+++ src/shared/datetime.js
@@ -2,3 +2,5 @@ import moment from 'moment'
 
 export const currentDateTime = () =>
   moment.utc().format('DDHHmm[Z] MMM YY').toUpperCase()
+
+export const fromNow = dtg => dtg && moment(dtg, 'DDHHmmZ MMM YYYY').fromNow()
