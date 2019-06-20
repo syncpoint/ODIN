59356b435b0988fb306d444ff916065e16839685
dehmer <horst.dehmer@gmail.com>
2019-06-20 20:45:50 +0200

    concentrated leaflet code in one place.

--- src/renderer/leaflet/L.GeoJSON.Symbols.js
+++ src/renderer/leaflet/L.GeoJSON.Symbols.js
@@ -8,7 +8,7 @@ import { K } from '../../shared/combinators'
 import selection from '../components/App.selection'
 import evented from '../evented'
 import { fromNow } from '../../shared/datetime'
-import gfxOptions from './gfx-options'
+import vectorFactories from './vector-factories'
 
 const MODIFIER_MAP = {
   f: 'reinforcedReduced',
@@ -179,115 +179,25 @@ const moveFeature = function (id, lat, lng) {
   marker.setLatLng(L.latLng(lat, lng))
 }
 
-const functionId = sidc => sidc.substring(4, 10)
 
-L.Area = L.Polygon.extend({
-  initialize (latlngs, options, properties) {
-    L.Polygon.prototype.initialize.call(this, latlngs, options)
-    this.properties = properties
-  },
-
-  onAdd (map) {
-    L.Polygon.prototype.onAdd.call(this, map)
-
-    const key = functionId(this.properties.sidc)
-    const options = gfxOptions[key]
-
-    this.label = K(L.SVG.create('text'))(text => {
-      const lines = []
-      if (options && options.type) lines.push(options.type)
-      if (this.properties.t) lines.push(this.properties.t)
-
-      const point = this.centerOfMass()
-      text.textContent = lines[0]
-      text.setAttribute('x', point[0])
-      text.setAttribute('y', point[1])
-      text.setAttribute('text-anchor', 'middle')
-
-      lines.splice(1).forEach(line => {
-        const tspan = L.SVG.create('tspan')
-        tspan.textContent = line
-        tspan.setAttribute('text-anchor', 'middle')
-        tspan.setAttribute('dy', '1.2em')
-        tspan.setAttribute('x', point[0])
-        text.appendChild(tspan)
-      })
-
-      const group = this._path.parentElement
-      group.appendChild(text)
-    })
-
-    this.updateLabelPosition()
-  },
-
-  onRemove (map) {
-    const group = this._path.parentElement
-    if (this.label) group.removeChild(this.label)
-    L.Polygon.prototype.onRemove.call(this, map)
-  },
-
-  redraw () {
-    L.Polygon.prototype.redraw.call(this)
-    this.updateLabelPosition()
-  },
-
-  updateLabelPosition () {
-    if (!this.label) return
-    const point = this.centerOfMass()
-    this.label.setAttribute('x', point[0])
-    this.label.setAttribute('y', point[1])
-
-    Array.from(this.label.childNodes)
-      .splice(1)
-      .forEach(child => child.setAttribute('x', point[0]))
-  },
-
-  _project () {
-    L.Polygon.prototype._project.call(this)
-    this.updateLabelPosition()
-  },
-
-  /**
-   * Optimization.
-   * Mostly Polygon.getCenter() without layerPointToLatLng().
-   *
-   * @returns layer point instead of latlng
-   */
-  centerOfMass () {
-    const points = this._rings[0]
-    const len = points.length
-    if (!len) return null
-
-    // polygon centroid algorithm; only uses the first ring if there are multiple
-    let area = 0
-    let x = 0
-    let y = 0
-
-    for (let i = 0, j = len - 1; i < len; j = i++) {
-      const f = points[i].y * points[j].x - points[j].y * points[i].x
-      x += (points[i].x + points[j].x) * f
-      y += (points[i].y + points[j].y) * f
-      area += f * 3
-    }
-
-    if (area === 0) return points[0]
-    else return [x / area, y / area]
-  }
-})
-
-const GEOMETRIES = {
-  Polygon: (geojson, options) => {
-    const geometry = geojson.type === 'Feature' ? geojson.geometry : geojson
-    const coords = geometry ? geometry.coordinates : null
-    const latlngs = L.GeoJSON.coordsToLatLngs(coords, 1)
-    const polygon = new L.Area(latlngs, options, geojson.properties)
-    return polygon
-  }
-}
+// const GEOMETRIES = {
+//   Polygon: (geojson, options) => {
+//     const geometry = geojson.type === 'Feature' ? geojson.geometry : geojson
+//     const coords = geometry ? geometry.coordinates : null
+//     const latlngs = L.GeoJSON.coordsToLatLngs(coords, 1)
+//     const polygon = new L.Area(latlngs, options, geojson.properties)
+//     return polygon
+//   }
+// }
 
 const geometryToLayer = function (geojson, options) {
-  const geometry = geojson.type === 'Feature' ? geojson.geometry : geojson
-  const factory = GEOMETRIES[geometry.type] || L.GeoJSON.geometryToLayer
+  // Either we find a SIDC/function specific layer factory or
+  // we use generic Leaflet method.
+  const { sidc } = geojson.properties
+  const functionId = sidc ? sidc.substring(4, 10) : '------'
+  const factory = vectorFactories[functionId] || L.GeoJSON.geometryToLayer
+
+  if (typeof factory !== 'function') return
   return factory(geojson, options)
 }
 
--- src/renderer/leaflet/index.js
+++ src/renderer/leaflet/index.js
@@ -1,11 +1,14 @@
-/**
- * Expose more ES6 idiomatic Leaflet API.
- */
+import 'leaflet/dist/leaflet.css'
+import 'leaflet.pm'
+import 'leaflet.pm/dist/leaflet.pm.css'
+import './leaflet-icons'
+import './L.Area'
+import './L.GeoJSON.Symbols'
 
-import { K } from '../shared/combinators'
+import { K } from '../../shared/combinators'
 
 // layers :: L.Map -> [L.Layer]
-const layers = map => {
+export const layers = map => {
   return K([])(layers => map.eachLayer(layer => layers.push(layer)))
 }
 
@@ -13,11 +16,6 @@ const layers = map => {
  * HTML `style` attributes for all matching layers (</div>) panes.
  * layerPanes :: (</div> a) => (L.Layer -> boolean) -> L.Map -> [a]
  */
-const panes = predicate => map => layers(map)
+export const panes = predicate => map => layers(map)
   .filter(predicate)
   .map(layer => layer.getPane())
-
-export default {
-  layers,
-  panes
-}
