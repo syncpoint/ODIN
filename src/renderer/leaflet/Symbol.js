63e7b99dd21ab97f900f898e7429928779ce82ec
dehmer <horst.dehmer@gmail.com>
2019-08-15 20:49:19 +0200

    symbol: edit mode.

--- src/renderer/leaflet/Symbol.js
+++ src/renderer/leaflet/Symbol.js
@@ -2,6 +2,7 @@ import L from 'leaflet'
 import ms from 'milsymbol'
 import { fromNow } from '../../shared/datetime'
 import { K } from '../../shared/combinators'
+import selection from '../components/App.selection'
 
 const MODIFIER_MAP = {
   f: 'reinforcedReduced',
@@ -23,25 +24,43 @@ const modifiers = feature => Object.entries(feature.properties)
   .reduce((acc, [key, value]) => K(acc)(acc => (acc[MODIFIER_MAP[key]] = value)), {})
 
 const symbolOptions = feature => ({
-  size: 34,
-  colorMode: 'Light', // default: light
-  simpleStatusModifier: true,
-  ...modifiers(feature)
+  standard: {
+    size: 34,
+    colorMode: 'Light', // default: light
+    simpleStatusModifier: true,
+    ...modifiers(feature)
+  },
+  highlighted: {
+    size: 34,
+    colorMode: 'Light', // default: light
+    simpleStatusModifier: true,
+    monoColor: 'white',
+    outlineColor: 'black',
+    outlineWidth: 6,
+    ...modifiers(feature)
+  }
 })
 
-const symbol = feature => {
+const symbol = (feature, options) => {
   const sidc = feature.properties.sidc
-  return new ms.Symbol(sidc, symbolOptions(feature))
+  return new ms.Symbol(sidc, options)
 }
 
 const options = {
-  draggable: true,
+  draggable: false, // only draggable in edit mode
   autoPan: true
 }
 
 const initialize = function (feature, options) {
   options = options || {}
-  options.icon = icon(symbol(feature))
+
+  // Prepare standard and highlighted icons:
+  this.icons = {}
+  Object.keys(symbolOptions(feature)).forEach(key => {
+    this.icons[key] = icon(symbol(feature, symbolOptions(feature)[key]))
+  })
+
+  options.icon = this.icons.standard
 
   L.Util.setOptions(this, options)
 
@@ -54,10 +73,53 @@ const initialize = function (feature, options) {
 
 const onAdd = function (map) {
   L.Marker.prototype.onAdd.call(this, map)
+  this.on('click', this.select, this)
+}
+
+const onRemove = function (map) {
+  this.off('click', this.select, this)
+  L.Marker.prototype.onRemove.call(this, map)
+}
+
+const select = function () {
+  selection.select(this.feature)
+  this.setIcon(this.icons.highlighted)
+  // TODO: only call this.edit() when not read-only
+  this.edit()
+}
+
+const deselect = function () {
+  this.dragging.disable()
+  this.setIcon(this.icons.standard)
+}
+
+const edit = function () {
+  // Remember original position in case dragging is cancelled:
+  this.latlng = this.getLatLng()
+
+  const editor = {
+    dispose: () => this.deselect(),
+    commit: () => {
+      const { lat, lng } = this.getLatLng()
+      const geometry = { type: 'Point', coordinates: [lng, lat] }
+      this.feature.updateGeometry(geometry)
+    },
+    rollback: () => {
+      this.deselect()
+      this.setLatLng(this.latlng)
+    }
+  }
+
+  this.dragging.enable()
+  this._map.tools.edit(editor)
 }
 
 L.Feature.Symbol = L.Marker.extend({
   options,
   initialize,
-  onAdd
+  onAdd,
+  onRemove,
+  select,
+  deselect,
+  edit
 })
