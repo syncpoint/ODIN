c4496f6a75a604088dbbc4ba76fd8474434a6383
dehmer <horst.dehmer@gmail.com>
2019-08-16 11:44:30 +0200

    SVG: don't fill paths, because of pointer events.

--- src/renderer/leaflet/Polyline.js
+++ src/renderer/leaflet/Polyline.js
@@ -5,7 +5,7 @@ L.Feature.Polyline = L.Feature.Polystar.extend({
 
   createShape (options) {
     const group = L.SVG.create('g')
-    const outlinePath = L.SVG.path({ 'stroke-width': 10, stroke: 'black', 'opacity': 0.0 })
+    const outlinePath = L.SVG.path({ 'stroke-width': 10, stroke: 'black', fill: 'none', 'opacity': 0.0 })
     const linePath = L.SVG.path({ 'stroke-width': 2, stroke: 'black', fill: 'none' })
 
     // TODO: check flag options.interactive
