7747dada81198e0e5d46b75c5b4e5687736f7094
dehmer <horst.dehmer@gmail.com>
2019-08-16 11:47:00 +0200

    SVG: don't fill boundary, because of pointer events.

--- src/renderer/leaflet/TACGRP/TACGRP.C2GM.GNL.LNE.BNDS.js
+++ src/renderer/leaflet/TACGRP/TACGRP.C2GM.GNL.LNE.BNDS.js
@@ -20,7 +20,7 @@ L.Feature['G*G*GLB---*****'] = L.Feature.Polyline.extend({
   createShape (options) {
     const id = uuid()
     const group = L.SVG.create('g')
-    const outlinePath = L.SVG.path({ 'stroke-width': 10, stroke: 'black', 'opacity': 0.0 })
+    const outlinePath = L.SVG.path({ 'stroke-width': 10, stroke: 'black', fill: 'none', 'opacity': 0.0 })
     const linePath = L.SVG.path({ 'stroke-width': 2, stroke: 'black', fill: 'none', mask: `url(#mask-${id})` })
 
     // TODO: check flag options.interactive
