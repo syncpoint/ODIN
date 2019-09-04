9a9454946bf1f2d9466aed5597f105331df57a72
dehmer <horst.dehmer@gmail.com>
2019-09-04 12:01:43 +0200

    spotlight/features: ES projection.

--- src/renderer/model/spotlight-features.js
+++ src/renderer/model/spotlight-features.js
@@ -3,11 +3,13 @@ import React from 'react'
 import { ListItemText, ListItemAvatar, Avatar } from '@material-ui/core'
 import L from 'leaflet'
 import ms from 'milsymbol'
-import layerStore from '../stores/layer-store'
+import store from '../stores/layer-store'
 import evented from '../evented'
-
+import { ResourceNames } from './resource-names'
 
 export default register => {
+  let replaying = true
+  let state = {}
   const contributor = new EventEmitter()
   const term = register(contributor)
 
@@ -16,37 +18,56 @@ export default register => {
     const symbolOptions = { simpleStatusModifier: true }
     const url = new ms.Symbol(feature.properties.sidc, symbolOptions).asCanvas().toDataURL()
     return {
-      key: `feature://${layerId}/${featureId}`,
+      key: ResourceNames.featureId(layerId, featureId),
       avatar: (
         <ListItemAvatar>
           <Avatar src={ url } style={{ borderRadius: 0, width: '15%', height: '15%' }} />
         </ListItemAvatar>
       ),
-      text: <ListItemText primary={ feature.title } secondary={ `${layerName.toUpperCase()}` }/>,
-      action: () => evented.emit('map.center', L.latLng(lat, lng))
+      text: <ListItemText primary={ feature.title } secondary={ `${layerName}` }/>,
+      action: () => evented.emit('map.center', L.latLng(lat, lng)),
+      delete: () => store.deleteFeature(layerId)([featureId])
     }
   }
 
-  const contribution = () => Object.entries(layerStore.state())
-    .reduce((acc, [layerId, { name: layerName, show, features }]) => {
-      if (!show) return acc
-      if (!term()) return acc
+  const contribution = () => {
+    if (!term()) return []
 
-      const items = Object.entries(features)
-        .filter(([_, feature]) => feature.title)
-        .filter(([_, feature]) => feature.properties.sidc)
-        .filter(([_, feature]) => feature.geometry)
-        .filter(([_, feature]) => feature.geometry.type === 'Point')
-        .filter(([_, feature]) => feature.title.search(new RegExp(term(), 'i')) !== -1)
-        .sort((a, b) => a[1].title.localeCompare(b[1].title))
-        .map(featureItem(layerId, layerName))
+    return Object.entries(state)
+      .filter(([_, layer]) => layer.show)
+      .reduce((acc, [layerId, { name, features }]) => {
+        const items = Object.entries(features)
+          .filter(([_, feature]) => feature.title)
+          .filter(([_, feature]) => feature.properties.sidc)
+          .filter(([_, feature]) => feature.geometry)
+          .filter(([_, feature]) => feature.geometry.type === 'Point')
+          .filter(([_, feature]) => feature.title.search(new RegExp(term(), 'i')) !== -1)
+          .sort((a, b) => a[1].title.localeCompare(b[1].title))
+          .map(featureItem(layerId, name))
 
-      // TODO: limit item count?
-      return acc.concat(items)
-    }, [])
+        // TODO: limit item count?
+        return acc.concat(items)
+      }, [])
+  }
 
   contributor.updateFilter = () => contributor.emit('updated', contribution())
-  layerStore.on('event', event => {
+
+  const handlers = {
+    'snapshot': ({ snapshot }) => (state = snapshot),
+    'replay-ready': () => (replaying = false),
+    'layer-added': ({ layerId, name, show }) => (state[layerId] = { name, show, features: {} }),
+    'layer-deleted': ({ layerId }) => delete state[layerId],
+    'layer-hidden': ({ layerId }) => (state[layerId].show = false),
+    'layer-shown': ({ layerId }) => (state[layerId].show = true),
+    'feature-added': ({ layerId, featureId, feature }) => (state[layerId].features[featureId] = feature),
+    'feature-updated': ({ layerId, featureId, feature }) => (state[layerId].features[featureId] = feature),
+    'feature-deleted': ({ layerId, featureId }) => delete state[layerId].features[featureId]
+  }
+
+  store.register(event => {
+    if (!handlers[event.type]) return
+    handlers[event.type](event)
+    if (replaying) return
     contributor.emit('updated', contribution())
   })
 }
