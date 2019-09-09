0aa5a0c5adf5b85a0db71756c6de682138131399
dehmer <horst.dehmer@gmail.com>
2019-09-09 13:14:42 +0200

    added some named areas.

--- src/renderer/leaflet/TACGRP/TACGRP.C2GM.js
+++ src/renderer/leaflet/TACGRP/TACGRP.C2GM.js
@@ -8,12 +8,19 @@ L.Feature['G*G*GAD---'] = L.Feature.NamedArea.extend({ name: 'DZ' })
 L.Feature['G*G*GAX---'] = L.Feature.NamedArea.extend({ name: 'EZ' })
 L.Feature['G*G*GAP---'] = L.Feature.NamedArea.extend({ name: 'PZ' })
 L.Feature['G*G*GAL---'] = L.Feature.NamedArea.extend({ name: 'LZ' })
-
-// OFFENSE (TACGRP.C2GM.OFF.ARS)
 L.Feature['G*G*OAK---'] = L.Feature.NamedArea.extend({ name: 'ATK' })
 L.Feature['G*G*OAO---'] = L.Feature.NamedArea.extend({ name: 'OBJ' })
-
-// SPECIAL (TACGRP.C2GM.SPL)
 L.Feature['G*G*SAO---'] = L.Feature.NamedArea.extend({ name: 'AO' })
 L.Feature['G*G*SAN---'] = L.Feature.NamedArea.extend({ name: 'NAI' })
 L.Feature['G*G*SAT---'] = L.Feature.NamedArea.extend({ name: 'TAI' })
+L.Feature['G*G*DAB---'] = L.Feature.Polygon // TODO: needs echelon
+L.Feature['G*F*ATS---'] = L.Feature.NamedArea.extend({ name: 'SMOKE' }) // TODO: W/W1
+L.Feature['G*S*AR----'] = L.Feature.NamedArea.extend({ name: 'FARP' }) // TODO: W/W1
+
+// NOTE: No distinction: IRREGULAR/RECTANGULAR, but no CIRCULAR
+L.Feature['G*F*ACFI--'] = L.Feature.NamedArea.extend({ name: 'FFA' }) // TODO: W/W1
+L.Feature['G*F*ACFR--'] = L.Feature.NamedArea.extend({ name: 'FFA' }) // TODO: W/W1
+L.Feature['G*F*ACNI--'] = L.Feature.NamedArea.extend({ name: 'NFA' }) // TODO: W/W1, fill pattern
+L.Feature['G*F*ACNR--'] = L.Feature.NamedArea.extend({ name: 'NFA' }) // TODO: W/W1, fill pattern
+L.Feature['G*F*ACRI--'] = L.Feature.NamedArea.extend({ name: 'RFA' }) // TODO: W/W1
+L.Feature['G*F*ACRR--'] = L.Feature.NamedArea.extend({ name: 'RFA' }) // TODO: W/W1
