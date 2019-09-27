24aeab54ee4b76fdc991e951654eb29b4003a55a
dehmer <horst.dehmer@gmail.com>
2019-09-27 17:00:30 +0200

    PolygonArea: clean-up.

--- src/renderer/leaflet/PolygonArea.js
+++ src/renderer/leaflet/PolygonArea.js
@@ -2,27 +2,18 @@ import L from 'leaflet'
 import selection from '../components/App.selection'
 import { fromNow } from '../../shared/datetime'
 import polygonShape from './polygon-shape'
+import GeoJSON from './GeoJSON'
 import evented from '../evented'
 import { findSpecificItem } from '../stores/feature-store'
 
-// GeoJSON geometry helper.
-const Geometry = geometry => {
-  const { type, coordinates } = geometry
 
-  const latlng = () => {
-    switch (type) {
-      case 'Point': return L.latLng(coordinates[1], coordinates[0])
-      case 'LineString': return coordinates.map(([lon, lat]) => L.latLng(lat, lon))
-      // NOTE: first ring only:
-      case 'Polygon': return coordinates.map(ring => ring.map(([lon, lat]) => L.latLng(lat, lon)))
-    }
-  }
-
-  return { latlng }
-}
-
+/**
+ *
+ */
 const initialize = function (feature, renderOptions, options) {
   this.feature = feature
+
+  // TODO: can we merge renderOptions and options?
   this.renderOptions = renderOptions
   L.setOptions(this, options)
 
@@ -37,83 +28,87 @@ const initialize = function (feature, renderOptions, options) {
     if (properties.w) evented.emit('OSD_MESSAGE', { slot: 'B3', message: fromNow(properties.w) })
   }
 
-  this.mouseout = () => {
-    evented.emit('OSD_MESSAGE', { slot: 'B1', message: '' })
-    evented.emit('OSD_MESSAGE', { slot: 'B2', message: '' })
-    evented.emit('OSD_MESSAGE', { slot: 'B3', message: '' })
-  }
+  this.mouseout = () =>
+    ['B1', 'B2', 'B3'].forEach(slot => evented.emit('OSD_MESSAGE', { slot, message: '' }))
 }
 
+
+/**
+ *
+ */
 const beforeAdd = function (map) {
-  this.renderer = map.getRenderer(this)
+  this._map = map
+  this._renderer = map.getRenderer(this)
 }
 
-const onAdd = function (map) {
-  this.zoomend = () => this.onGeometry && this.onGeometry(this.feature.geometry)
-  this.click = () => this.edit(map)
 
-  map.on('zoomend', this.zoomend)
-  this.on('click', this.click)
+/**
+ *
+ */
+const onAdd = function (map) {
+  map.on('zoomend', this.updateShape, this)
+  this.on('click', this.edit, this)
   this.on('mouseover', this.mouseover)
   this.on('mouseout', this.mouseout)
 
-  const shapeOptions = {
-    styles: this.renderOptions.styles(this.feature),
-    labels: this.options.hideLabels ? [] : this.renderOptions.labels(this.feature)
+  const createShape = () => {
+    const shapeOptions = {
+      styles: this.renderOptions.styles(this.feature),
+      labels: this.options.hideLabels ? [] : this.renderOptions.labels(this.feature),
+      interactive: this.options.interactive
+    }
+
+    const rings = GeoJSON.latlng(this.feature.geometry)
+    const points = rings.map(ring => ring.map(latlng => map.latLngToLayerPoint(latlng)))
+    const shape = polygonShape(map.getRenderer(this), points, shapeOptions)
+    if (this.options.interactive) this.addInteractiveTarget(shape.element)
+    return shape
   }
 
-  const rings = Geometry(this.feature.geometry).latlng()
-  const points = rings.map(ring => ring.map(latlng => map.latLngToLayerPoint(latlng)))
-  shapeOptions.interactive = this.options.interactive
-  this.shape = polygonShape(map.getRenderer(this), points, shapeOptions)
-
-  this.onLatLngs = latlngs => {
-    latlngs = [...latlngs, latlngs[0]]
-    const layerPoints = latlngs.map(latlng => map.latLngToLayerPoint(latlng))
-    this.shape = this.shape.updatePoints([layerPoints], this.options.lineSmoothing)
-  }
-
-  this.onGeometry = geometry => {
-    const rings = Geometry(geometry).latlng()
-    const layerPoints = rings.map(ring => ring.map(latlng => map.latLngToLayerPoint(latlng)))
-    this.shape = this.shape.updatePoints(layerPoints, this.options.lineSmoothing)
-  }
-
-  if (this.options.interactive) this.addInteractiveTarget(this.shape.element)
-  this.onGeometry(this.feature.geometry)
-
-  // FIXME: why do we need this here?
+  this.shape = createShape()
   this.shape = this.shape.updateStyles(this.renderOptions.styles(this.feature))
+  this.updateShape()
 }
 
+
+/**
+ *
+ */
 const onRemove = function (map) {
   if (this.options.interactive) this.removeInteractiveTarget(this.shape.element)
-  this.off('click', this.click)
-  map.off('zoomend', this.zoomend)
-  delete this.click
-  delete this.zoomend
+  this.off('click', this.edit, this)
+  map.off('zoomend', this.updateShape, this)
   this.shape.dispose()
   map.tools.dispose() // dispose editor/selection tool
 }
 
-const edit = function (map) {
 
+/**
+ *
+ */
+const edit = function () {
   if (selection.isSelected(this.urn)) return
   selection.select(this.urn)
 
   const callback = event => {
+    const onLatLngs = latlngs => {
+      latlngs = [...latlngs, latlngs[0]]
+      const layerPoints = latlngs.map(latlng => this._map.latLngToLayerPoint(latlng))
+      this.shape = this.shape.updatePoints([layerPoints], this.options.lineSmoothing)
+    }
+
     switch (event.type) {
-      case 'latlngs': return this.onLatLngs(event.latlngs)
+      case 'latlngs': return onLatLngs(event.latlngs)
       case 'geometry': return this.options.update({ geometry: event.geometry })
     }
   }
 
   this.markerGroup = new L.Feature.MarkerGroup(this.feature.geometry, callback)
-  this.markerGroup.addTo(map)
+  this.markerGroup.addTo(this._map)
 
   const editor = {
     dispose: () => {
-      map.removeLayer(this.markerGroup)
+      this._map.removeLayer(this.markerGroup)
       delete this.markerGroup
       if (selection.isSelected(this.urn)) {
         selection.deselect()
@@ -121,9 +116,23 @@ const edit = function (map) {
     }
   }
 
-  map.tools.edit(editor)
+  this._map.tools.edit(editor)
 }
 
+
+/**
+ * Update shape's geometry/points.
+ */
+const updateShape = function () {
+  const rings = GeoJSON.latlng(this.feature.geometry)
+  const layerPoints = rings.map(ring => ring.map(latlng => this._map.latLngToLayerPoint(latlng)))
+  this.shape = this.shape.updatePoints(layerPoints, this.options.lineSmoothing)
+}
+
+
+/**
+ *
+ */
 const updateData = function (feature) {
   this.feature = feature
 
@@ -132,7 +141,7 @@ const updateData = function (feature) {
   const labels = this.options.hideLabels ? [] : this.renderOptions.labels(this.feature)
   this.shape = this.shape.updateLabels(labels)
 
-  this.onGeometry && this.onGeometry(feature.geometry)
+  this.updateShape()
   this.markerGroup && this.markerGroup.updateGeometry(feature.geometry)
 }
 
@@ -143,5 +152,6 @@ L.Feature.PolygonArea = L.Layer.extend({
   onRemove,
 
   edit,
+  updateShape,
   updateData
 })
