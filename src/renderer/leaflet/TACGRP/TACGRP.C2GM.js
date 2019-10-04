f2c86a9d2f902a62077f822abe76dd9ae434ad69
dehmer <horst.dehmer@gmail.com>
2019-10-04 07:21:32 +0200

    new: TACGRP.Polyline

--- src/renderer/leaflet/TACGRP/TACGRP.C2GM.js
+++ src/renderer/leaflet/TACGRP/TACGRP.C2GM.js
@@ -1,71 +1,14 @@
 import L from 'leaflet'
 import '../PolygonArea'
 import './PolygonArea'
+import './Polyline'
+import { styles } from './styles'
 import './TACGRP.C2GM.GNL.ARS.SRHARA'
 import './TACGRP.C2GM.GNL.LNE.BNDS'
 import './TACGRP.C2GM.OFF.LNE.AXSADV.ABN'
 import './TACGRP.C2GM.OFF.LNE.AXSADV.AVN'
 import './TACGRP.C2GM.OFF.LNE.AXSADV.GRD.MANATK'
 import './TACGRP.C2GM.OFF.LNE.AXSADV.GRD.SUPATK'
-
-const ColorSchemes = {
-  dark: {
-    red: 'RGB(200, 0, 0)',
-    blue: 'RGB(0, 107, 140)',
-    green: 'RGB(0, 160, 0)',
-    // recommended: 'RGB(225, 220, 0)'
-    // more orange than yellow: 'RGB(225, 127, 0)'
-    yellow: 'RGB(225, 127, 0)',
-    purple: 'RGB(80, 0, 80)'
-  },
-  medium: {
-    red: 'RGB(255, 48, 49)',
-    blue: 'RGB(0, 168, 220)',
-    green: 'RGB(0, 226, 0)',
-    yellow: 'RGB(255, 255, 0)',
-    purple: '128, 0, 128'
-  }
-}
-
-// STYLES
-// * clipping: none | mask | backdrop
-// * stroke: path stroke color
-// * patternStroke: fill pattern stroke color
-// * strokeWidth: path stroke with
-// * strokeDashArray: path stroke dash pattern
-// * fill: none | diagonal
-
-const styles = feature => {
-  const colorSchemes = ColorSchemes['medium']
-
-  const { sidc, n } = feature.properties
-  const stroke = () => {
-    if (n === 'ENY') return colorSchemes.red
-
-    const identity = sidc ? sidc[1] : 'U' // identity or U - UNKNOWN
-    switch (identity) {
-      case 'F': return colorSchemes.blue
-      case 'H': return colorSchemes.red
-      case 'N': return colorSchemes.green
-      case 'U': return colorSchemes.yellow
-      default: return 'black'
-    }
-  }
-
-  const strokeDashArray = () => {
-    const status = sidc ? sidc[3] : 'P' // status or P - PRESENT
-    if (status === 'A') return '15 5'
-  }
-
-  return {
-    clipping: 'none',
-    stroke: stroke(),
-    patternStroke: stroke(),
-    strokeWidth: 3,
-    strokeDashArray: strokeDashArray(),
-    fill: 'none'
-  }
-}
 
 const effectiveLine = properties => {
   if (!properties.w && !properties.w1) return null
