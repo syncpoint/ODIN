bc0353c80d53c7a964a339d5e8351df37f153184
dehmer <horst.dehmer@gmail.com>
2019-10-06 15:04:58 +0200

    TSK: upgraded CNT.

--- src/renderer/leaflet/TACGRP/TACGRP.TSK.CNT.js
+++ src/renderer/leaflet/TACGRP/TACGRP.TSK.CNT.js
@@ -1,46 +1,52 @@
 import L from 'leaflet'
-import '../Corridor2Point'
+import * as R from 'ramda'
+import { line, calcStruts2 } from './shapes/geo-helper'
+import { shape } from './shapes/shape'
+import './Corridor'
 
-// TACTICAL GRAPHICS / TASKS / CONTAIN
-L.Feature['G*T*J-----'] = L.Corridor2Point.extend({
+const arc = (c, r) => xs => xs.map(x => L.point(r * Math.cos(x) + c.x, r * Math.sin(x) + c.y))
 
-  path ({ A, B, width, finalBearing }) {
-    const halfWidth = width / 2
-    const tenthWidth = width / 10
+L.Feature['G*T*J-----'] = L.TACGRP.Corridor.extend({
 
-    const outerArc = []
-    const innerArc = []
-    const orientAngle = finalBearing - 90
-    const sizeAngle = 180
+  _shape (group) {
+    const options = { ...this._shapeOptions }
+    options.styles.clipping = 'mask'
 
-    for (let angle = orientAngle; angle <= orientAngle + sizeAngle; angle += (180 / 32)) {
-      outerArc.push(B.destinationPoint(halfWidth, angle))
-      innerArc.push(B.destinationPoint(halfWidth * 0.8, angle))
-    }
+    /* eslint-disable */
+    return shape(group, options, {
+      points: ({ center, envelope }) => {
+        const angle = (line(center).angle() - 90) / 180 * Math.PI
+        const s = calcStruts2(center, envelope)([0.1])
+        const xs = R.range(0, 33).map(x => angle + (x / 32) * Math.PI)
+        const oa = arc(center[0], s[0].d / 2)(xs)
+        const ia = arc(center[0], s[0].d / 3)(xs)
+        const spikes = R.zip(oa, ia).filter((_, i) => i % 4 === 0)
 
-    const spikes = []
-    for (let i = 0; i < innerArc.length; i++) {
-      if (i % 4 === 0) spikes.push([outerArc[i], innerArc[i]])
-    }
-
-    const arrow = L.Shape.arrow(B, tenthWidth, finalBearing)
-    return [outerArc].concat(spikes, [[A, B], arrow])
+        return [
+          center,
+          oa, ...spikes,
+          [s[0].point(0.4), center[0], s[0].point(0.6)]
+        ]
+      }
+    })
   },
 
-  labelCount: 2,
-
-  label ({ A, A1, A2, B, initialBearing, finalBearing }) {
-    return [
-      {
-        text: 'ENY',
-        latlng: A.destinationPoint(A.distanceTo(B) / 2, initialBearing),
-        bearing: A.initialBearingTo(B)
+  _labels () {
+    return [{
+      placement: ({ center }) => line(center).point(0.5),
+      lines: ['ENY'],
+      'font-size': 18,
+      angle: ({ center }) => line(center).angle()
+    },
+    {
+      placement: ({ center, envelope }) => {
+        const centerLine = line(center)
+        const width = line(envelope[0]).d
+        return centerLine.point(-0.5 * width / centerLine.d)
       },
-      {
-        text: 'C',
-        latlng: B.destinationPoint(A1.distanceTo(A2) / 2, finalBearing),
-        bearing: finalBearing
-      }
-    ]
+      lines: ['C'],
+      'font-size': 18,
+      angle: ({ center }) => line(center).angle()
+    }]
   }
 })
--- src/renderer/leaflet/editors/index.js
+++ src/renderer/leaflet/editors/index.js
@@ -1,11 +1,8 @@
-import { corridorNPointEditor } from './CorridorNPoint'
 import { editor } from './Generic'
 import * as Shapes from './Shapes'
 
 export const editors = {}
 editors['generic'] = editor(Shapes.noShape)
-editors['2pt-corridor'] = editor(Shapes.corridor2PointShape)
 editors['orbit'] = editor(Shapes.orbitShape)
 editors['fan'] = editor(Shapes.fanShape)
 editors['arc'] = editor(Shapes.arcShape)
-editors['npt-corridor'] = corridorNPointEditor(Shapes.corridorNPointShape)
