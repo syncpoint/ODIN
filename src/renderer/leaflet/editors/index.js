622bb9f136f2386612d1f6716dc959693aebc558
dehmer <horst.dehmer@gmail.com>
2019-10-07 21:09:35 +0200

    clean-up: removed deprecated code.

--- src/renderer/leaflet/editors/index.js
+++
@@ -1,8 +0,0 @@
-import { editor } from './Generic'
-import * as Shapes from './Shapes'
-
-export const editors = {}
-editors['generic'] = editor(Shapes.noShape)
-editors['orbit'] = editor(Shapes.orbitShape)
-editors['fan'] = editor(Shapes.fanShape)
-editors['arc'] = editor(Shapes.arcShape)
