a479842a2cea43bacedd994ac015792e8d302c06
dehmer <horst.dehmer@gmail.com>
2019-10-07 13:55:32 +0200

    orbit area: editor.

--- src/renderer/leaflet/TACGRP/OrbitArea.js
+++ src/renderer/leaflet/TACGRP/OrbitArea.js
@@ -1,29 +1,36 @@
-/* eslint-disable */
-
 import L from 'leaflet'
 import './Feature'
 import { toLatLngs, toGeometry } from '../GeoJSON'
 import { line } from './shapes/geo-helper'
+import { FULCRUM } from './handle-types'
 
+/* eslint-disable object-property-newline */
 
 /**
  *
  */
 const orbitGeometry = (latlngs, width, alignment) => {
-  const [A, B] = latlngs
 
-  const initialBearing = A.initialBearingTo(B)
-  const finalBearing = A.finalBearingTo(B)
-  const offset = alignment === 'RIGHT' ? 90 : -90
+  const create = current => {
+    const { latlngs, width, alignment } = current
+    const [A, B] = latlngs
 
-  const A1 = A.destinationPoint(width, initialBearing + offset)
-  const B1 = B.destinationPoint(width, finalBearing + offset)
+    const initialBearing = A.initialBearingTo(B)
+    const finalBearing = A.finalBearingTo(B)
+    const offset = alignment === 'RIGHT' ? 90 : -90
 
-  return {
-    envelope: { A, B, A1, B1 },
-    width,
-    alignment
+    const A1 = A.destinationPoint(width, initialBearing + offset)
+    const B1 = B.destinationPoint(width, finalBearing + offset)
+
+    return {
+      copy: properties => create({ ...current, ...properties }),
+      A, B, A1, B1,
+      width,
+      alignment
+    }
   }
+
+  return create({ latlngs, width, alignment })
 }
 
 
@@ -38,20 +45,69 @@ L.TACGRP.OrbitArea = L.TACGRP.Feature.extend({
   _project () {
     const layerPoint = this._map.latLngToLayerPoint.bind(this._map)
 
-    const envelope = Object.entries(this._orbit.envelope).reduce((acc, [key, value]) => {
-      acc[key] = layerPoint(value)
-      return acc
-    }, {})
+    const A = layerPoint(this._orbit.A)
+    const B = layerPoint(this._orbit.B)
+    const A1 = layerPoint(this._orbit.A1)
+    const B1 = layerPoint(this._orbit.B1)
+
 
     this._shape.updateFrame({
-      envelope,
-      width: line([envelope.A, envelope.A1]).d,
+      A, B, A1, B1,
+      width: line([A, A1]).d,
       alignment: this._orbit.alignment
     })
   },
 
   _editor () {
     const layer = new L.Feature.Handles().addTo(this._map)
+    let current = this._orbit
+
+    const handlers = {
+      A: {
+        latlng: orbit => orbit.A,
+        orbit: latlng => current.copy({ latlngs: [latlng, current.B] })
+      },
+      B: {
+        latlng: orbit => orbit.B,
+        orbit: latlng => current.copy({ latlngs: [current.A, latlng] })
+      },
+      A1: {
+        latlng: orbit => orbit.A1,
+        orbit: latlng => current.copy({ width: current.A.distance(latlng) })
+      },
+      B1: {
+        latlng: orbit => orbit.B1,
+        orbit: latlng => current.copy({ width: current.B.distance(latlng) })
+      }
+    }
+
+    const update = (channel, orbit) => {
+      this._orbit = current = orbit
+      this._project()
+      Object.keys(handlers).forEach(id => handles[id].setLatLng(orbit[id]))
+
+      if (channel === 'dragend') {
+        const geometry = toGeometry('MultiPoint', [orbit.A, orbit.B])
+        const properties = {
+          geometry_width: orbit.width,
+          geometry_alignment: orbit.alignment
+        }
+
+        return this.options.update({ geometry, properties })
+      }
+    }
+
+    const handles = Object.entries(handlers).reduce((acc, [id, handler]) => {
+      const handleOptions = {
+        type: FULCRUM,
+        drag: ({ target }) => update('drag', handler.orbit(target.getLatLng())),
+        dragend: ({ target }) => update('dragend', handler.orbit(target.getLatLng()))
+      }
+
+      const latlng = handler.latlng(current)
+      acc[id] = layer.addHandle(latlng, handleOptions)
+      return acc
+    }, {})
 
     return {
       dispose: () => this._map.removeLayer(layer)
