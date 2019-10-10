2093c080d13f2ca0244816cb41c5d4e99e53ad83
dehmer <horst.dehmer@gmail.com>
2019-10-10 16:39:40 +0200

    MainPanel: moved some code (draw-feature).

--- src/renderer/components/mainPanel/MainPanel.js
+++ src/renderer/components/mainPanel/MainPanel.js
@@ -7,40 +7,7 @@ import FeatureSet from './FeatureSet'
 import featureSet from '../../model/mapPalette-featureSet'
 import Features from './Features'
 import { featureList } from '../../model/mapPalette-feature'
-import uuid from 'uuid-random'
-import evented from '../../evented'
-import ms from 'milsymbol'
-import layerStore from '../../stores/layer-store'
-import { findSpecificItem } from '../../stores/feature-store'
-import { ResourceNames } from '../../model/resource-names'
-import selection from '../App.selection'
-
-const geometryType = (descriptor, sidc) => {
-  const includes = type => descriptor.geometries.includes(type)
-  const validSymbol = () => new ms.Symbol(sidc, {}).isValid()
-
-  // Usually a point, but not always:
-  if (!descriptor.geometries) return validSymbol() ? 'point' : null
-  if (descriptor.geometries.length === 1) return descriptor.geometries[0]
-
-  // Guess-work...
-  if (includes('point') && validSymbol()) return 'point'
-
-  return null
-}
-
-const geometry = (geometryType, latlngs, properties) => {
-  if (geometryType === 'point') return { type: 'Point', coordinates: [latlngs.lng, latlngs.lat] }
-  const lineString = () => latlngs.map(({ lat, lng }) => [lng, lat])
-  const polygon = () => [lineString()]
-
-  switch (geometryType) {
-    case 'polygon': return { type: 'Polygon', coordinates: polygon() }
-    case 'line': return { type: 'LineString', coordinates: lineString() }
-    case 'corridor': return { type: 'LineString', coordinates: lineString(), ...properties }
-    case 'corridor-2pt': return { type: 'LineString', coordinates: lineString(), ...properties }
-  }
-}
+import drawShape from '../map/draw-feature'
 
 class MainPanel extends React.Component {
 
@@ -70,82 +37,21 @@ class MainPanel extends React.Component {
   }
 
   elementFeatureSelected (selectedFeatureIndex, parentId) {
+
     const { featureSet, indexCache, features } = this.state
     const setId = parentId === -1 ? indexCache : parentId
     const getFeatureFromSet = (selectedFeatureIndex, setId) => {
       const set = featureSet[setId]
       return set.features[selectedFeatureIndex]
     }
-    const feature = this.type === 'features' ? features[selectedFeatureIndex] : getFeatureFromSet(selectedFeatureIndex, setId)
-    const sidc = feature.sidc
-
-    // TODO: move to GeoJSON/Feature/SIDC helper.
-    const genericSIDC = sidc[0] + '*' + sidc[2] + '*' + sidc.substring(4, 15)
-    const featureDescriptor = findSpecificItem(genericSIDC)
-    const type = geometryType(featureDescriptor, sidc)
-
-    const geometryHint = () => evented.emit('OSD_MESSAGE', {
-      message: `Sorry, the feature's geometry is not supported, yet.`,
-      duration: 5000
-    })
 
     this.setState({ ...this.state, featureSet, selectedSetIndex: -1, selectedFeatureIndex, indexCache: setId })
-    if (!type) return geometryHint()
 
-    switch (type) {
-      case 'point': return evented.emit('tools.pick-point', {
-        prompt: 'Pick a location...',
-        picked: latlng => {
-          const featureId = uuid()
-          selection.preselect(ResourceNames.featureId('0', featureId))
-          layerStore.addFeature(0)(featureId, {
-            type: 'Feature',
-            geometry: geometry(type, latlng),
-            properties: { sidc }
-          })
-        }
-      })
-      case 'line':
-      case 'polygon': return evented.emit('tools.draw', {
-        geometryType: type,
-        prompt: `Draw a ${type}...`,
-        done: latlngs => {
-          const featureId = uuid()
-          selection.preselect(ResourceNames.featureId('0', featureId))
-          layerStore.addFeature(0)(featureId, {
-            type: 'Feature',
-            geometry: geometry(type, latlngs),
-            properties: { sidc }
-          })
-        }
-      })
-      case 'corridor': return evented.emit('tools.draw', {
-        geometryType: type,
-        prompt: `Draw a ${type}...`,
-        done: latlngs => {
-          const featureId = uuid()
-          selection.preselect(ResourceNames.featureId('0', featureId))
-          layerStore.addFeature(0)(featureId, {
-            type: 'Feature',
-            geometry: geometry(type, latlngs),
-            properties: { sidc, geometry_width: 1000 }
-          })
-        }
-      })
-      case 'corridor-2pt': return evented.emit('tools.draw', {
-        geometryType: 'line-2pt',
-        prompt: `Draw a corridor (2 points)...`,
-        done: latlngs => {
-          const featureId = uuid()
-          selection.preselect(ResourceNames.featureId('0', featureId))
-          layerStore.addFeature(0)(featureId, {
-            type: 'Feature',
-            geometry: geometry(type, latlngs),
-            properties: { sidc, geometry_width: 1000 }
-          })
-        }
-      })
-    }
+    const feature = this.type === 'features'
+      ? features[selectedFeatureIndex]
+      : getFeatureFromSet(selectedFeatureIndex, setId)
+
+    drawShape(feature.sidc)
   }
 
   selectNextElement (direction) {
