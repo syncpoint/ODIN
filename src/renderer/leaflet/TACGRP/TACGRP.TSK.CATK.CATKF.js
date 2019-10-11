2589349a41ff41d369e7bbaeb1d4245cf2ddff54
dehmer <horst.dehmer@gmail.com>
2019-10-11 12:34:27 +0200

    #136: double width for attack/axis of advance.

--- src/renderer/leaflet/TACGRP/TACGRP.TSK.CATK.CATKF.js
+++ src/renderer/leaflet/TACGRP/TACGRP.TSK.CATK.CATKF.js
@@ -14,30 +14,25 @@ L.Feature['G*T*KF----'] = L.TACGRP.Corridor.extend({
       const dw = line(envelope[0]).d
       const s0 = line(center.slice(0, 2))
 
-      // CATKF has a pretty complicated set of arrows.
-      // First we define a set of struts s(0) - s(n), starting from the tip.
-      // Then we calculate named points on these struts.
-      const s = calcStruts(center, envelope)([ 0.1, 0.28, 0.53, 0.69, 0.92 ])
-
-      // Interpolate points for corridor width (half of arrow width)
-      // TODO: remove/simplify shape when minimum width is below a certain limit
+      // CATKF has a pretty complicated set of 'arrows'.
+      const s = calcStruts(center, envelope)([ 0.2, 0.56, 1.06, 1.38, 1.84 ])
       const struts = envelope.map(line).slice(1)
 
       return [
         [
-          ...struts.map(s => s.point(0.75)).reverse(),
-          s[4].point(0.75), s[4].point(1),
-          s0.point(0.53 * (dw / s0.d)),
-          s[4].point(0), s[4].point(0.25),
-          ...struts.map(s => s.point(0.25))
+          ...struts.map(s => s.point(1)).reverse(),
+          s[4].point(1), s[4].point(1.5),
+          s0.point(1.06 * (dw / s0.d)),
+          s[4].point(-0.5), s[4].point(0),
+          ...struts.map(s => s.point(0))
         ],
         [
-          s[2].point(-0.5),
-          s[1].point(0),
-          s[1].point(1),
-          s[2].point(1.5)
+          s[2].point(-0.75),
+          s[1].point(-0.5),
+          s[1].point(1.5),
+          s[2].point(1.75)
         ],
-        [s0.point(0.1 * (dw / s0.d)), s0.point(0.28 * (dw / s0.d))],
+        [s0.point(0.2 * (dw / s0.d)), s0.point(0.56 * (dw / s0.d))],
         [center[0], s[0].point(0.45), s[0].point(0.55), center[0]]
       ]
     }
