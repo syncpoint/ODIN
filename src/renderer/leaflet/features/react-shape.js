c0494e3b88cc82b75e9c33b723668b8bc8561070
dehmer <horst.dehmer@gmail.com>
2019-10-14 21:46:23 +0200

    label placement (wip).

--- src/renderer/leaflet/features/react-shape.js
+++ src/renderer/leaflet/features/react-shape.js
@@ -4,7 +4,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import uuid from 'uuid-random'
 
-
 const translate = style => ({
   stroke: style['stroke'],
   strokeWidth: style['stroke-width'],
@@ -16,7 +15,18 @@ const translate = style => ({
 
 const Shape = props => {
   const { id, options, d } = props
+  const { placements } = props
   const className = options.interactive ? 'leaflet-interactive' : ''
+
+  const labels = (options.labels || []).map((label, index) => {
+    const {x, y} = placements[label.placement]
+    return (
+      // TODO: line 0 => text, line 1, 2, ... => tspan
+      <text key={index} fontSize={16} x={x} y={y} textAnchor='middle'>
+        {label.lines[0]}
+      </text>
+    )
+  })
 
   return (<>
     <defs>
@@ -26,7 +36,7 @@ const Shape = props => {
     <use xlinkHref={`#path-${id}`} {...translate(options.styles['outline'])}/>
     <use xlinkHref={`#path-${id}`} {...translate(options.styles['contrast'])}/>
     <use xlinkHref={`#path-${id}`} {...translate(options.styles['path'])}/>
-    <g></g>
+    { labels }
   </>)
 }
 
@@ -35,9 +45,12 @@ export const shape = (group, options, callbacks) => {
   const state = { id, options, callbacks }
   const render = () => ReactDOM.render(<Shape {...state}/>, group)
 
+  if ('object' === typeof options.labels) state.labels = options.labels
+
   return {
     updateFrame: frame => {
       const { closed, lineSmoothing } = state.options
+      state.placements = (callbacks.placements && callbacks.placements(frame)) || {}
       state.d = L.SVG.pointsToPath(callbacks.points(frame), closed, lineSmoothing)
       render()
     },
