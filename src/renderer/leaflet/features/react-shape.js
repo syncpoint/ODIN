4ce7a22564489ae0e2bfc00be6bd41e238a1fa4e
dehmer <horst.dehmer@gmail.com>
2019-10-18 09:02:55 +0200

    labels: white text outline for better contrast.

--- src/renderer/leaflet/features/react-shape.js
+++ src/renderer/leaflet/features/react-shape.js
@@ -123,14 +123,27 @@ const Label = React.forwardRef((props, ref) => {
   const { content, fontWeight } = lineProperties(lines[0])
 
   return (
-    <text ref={ref}
-          fontSize={16} x={x} y={y}
-          textAnchor={textAnchor} fontWeight={fontWeight} fontSize={fontSize}
-          alignmentBaseline={'central'}
-    >
-      {content}
-      {tspans()}
-    </text>
+    <g ref={ref}>
+      <text
+            fontSize={16} x={x} y={y}
+            textAnchor={textAnchor} fontWeight={fontWeight} fontSize={fontSize}
+            alignmentBaseline={'central'}
+            strokeWidth={2} stroke={'white'} fill={'none'}
+            strokeLinecap={'round'} strokeLinejoin={'round'}
+      >
+        {content}
+        {tspans()}
+      </text>
+      <text
+            fontSize={16} x={x} y={y}
+            textAnchor={textAnchor} fontWeight={fontWeight} fontSize={fontSize}
+            alignmentBaseline={'central'}
+            stroke={'none'} fill={'blank'}
+      >
+        {content}
+        {tspans()}
+      </text>
+    </g>
   )
 })
 
