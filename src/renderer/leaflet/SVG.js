951f02781b61c2386d263dff9192fd7bcf303d75
dehmer <horst.dehmer@gmail.com>
2019-10-18 09:44:35 +0200

    clean-up: removed deprecated code.

--- src/renderer/leaflet/SVG.js
+++ src/renderer/leaflet/SVG.js
@@ -5,37 +5,6 @@ import { svgPath, bezierCommand } from './smooth'
 const setAttribute = (o, [k, v]) => K(o)(o => o.setAttribute(k, v))
 L.SVG.setAttributes = o => attrs => Object.entries(attrs).reduce(setAttribute, o)
 
-// TODO: mostly deprecated; remove
-
-L.SVG.mask = attrs => L.SVG.setAttributes(L.SVG.create('mask'))(attrs)
-L.SVG.rect = attrs => L.SVG.setAttributes(L.SVG.create('rect'))(attrs)
-L.SVG.circle = attrs => L.SVG.setAttributes(L.SVG.create('circle'))(attrs)
-L.SVG.path = attrs => L.SVG.setAttributes(L.SVG.create('path'))(attrs)
-L.SVG.text = attrs => L.SVG.setAttributes(L.SVG.create('text'))(attrs)
-L.SVG.tspan = attrs => L.SVG.setAttributes(L.SVG.create('tspan'))(attrs)
-L.SVG.g = attrs => L.SVG.setAttributes(L.SVG.create('g'))(attrs)
-L.SVG.pattern = attrs => L.SVG.setAttributes(L.SVG.create('pattern'))(attrs)
-
-// TODO: deprecated; remove
-L.SVG.diagonalPattern = (id, styles) => {
-  const pattern = L.SVG.pattern({
-    id,
-    patternUnits: 'userSpaceOnUse',
-    width: 4,
-    height: 8,
-    patternTransform: 'rotate(-45)'
-  })
-
-  const patternPath = L.SVG.path({
-    stroke: styles.patternStroke || styles.stroke,
-    'stroke-width': 2,
-    d: 'M -1,2 l 6,0'
-  })
-
-  pattern.appendChild(patternPath)
-  return pattern
-}
-
 // inflate :: SVGRect => number => SVGRect-like
 L.SVG.inflate = (rect, delta) => ({
   x: rect.x - delta,
