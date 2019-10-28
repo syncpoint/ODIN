ea64b7af91a1b5fa16a7d9f62dbe52f9fa98d29d
Julian Kühnel <julian.kuehnel@syncpoint.io>
2019-10-28 09:56:25 +0100

    recently used und recently opened persited

--- src/renderer/stores/featureSetStore.js
+++ src/renderer/stores/featureSetStore.js
@@ -5,7 +5,7 @@ const xlsx = require('node-xlsx')
 
 const featureSetData = () => {
   const { app } = remote
-  const filename = path.join(app.getAppPath(), 'feature-sets.xls')
+  const filename = path.join(app.getAppPath(), 'feature-sets.xlsx')
 
   const worksheets = xlsx.parse(filename)
   const sets = worksheets[0].data.slice(1).reduce((acc, line) => {
