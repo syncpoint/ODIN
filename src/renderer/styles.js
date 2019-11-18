6126425da18745f44a78951fd11c4e807124fed7
dehmer <horst.dehmer@gmail.com>
2019-11-18 17:49:24 +0100

    support generic GeoJSON.

--- src/renderer/styles.js
+++ src/renderer/styles.js
@@ -1,8 +1,23 @@
 import symbolStyle from './styles/symbol-style'
 import tacgrpStyle from './styles/tacgrp-style'
+import { Style, Stroke } from './styles/predef'
 
 const featureStyle = modeOptions => (feature, resolution) => {
   if (!feature.getGeometry()) return null
+
+  const { sidc } = feature.getProperties()
+  if (!sidc) {
+    if (!feature.getStyle()) {
+      const style = [ Style.of({
+        // TODO: tolerance should depend on resolution.
+        geometry: feature.getGeometry().simplify(100), // tolerance [meters]
+        stroke: Stroke.of({ color: 'black' })
+      })]
+      feature.setStyle(style)
+      return style
+    }
+  }
+
   const fn = feature.getGeometry().getType() === 'Point' ? symbolStyle : tacgrpStyle
   return fn(modeOptions)(feature, resolution)
 }
