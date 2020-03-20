ad644bf872608a6c0b750b18ce9dca155d36b043
Gerald Peel <gerald.p83@outlook.com>
2020-03-20 20:52:16 +0100

    added homedir as default projectpath for tests.

--- test/hooks.js
+++ test/hooks.js
@@ -1,12 +1,15 @@
 import { Application } from 'spectron'
 import electron from 'electron'
+import os from 'os'
 import path from 'path'
+
+const projectPath = os.homedir()
 
 module.exports = {
   async startApp () {
     const app = await new Application({
       path: electron,
-      args: [path.join(__dirname, '..'), '--noDevServer']
+      args: [path.join(__dirname, '..'), '--noDevServer', `projectPath=${projectPath}`]
     }).start()
     return app
   },
