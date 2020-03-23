38879fe9b301f7c530265d254b313c1110d116b0
Thomas Halwax <thomas.halwax@syncpoint.io>
2020-03-23 15:17:26 +0100

    added accelerator "M" key for project management

--- src/main/menu/projects-menu.js
+++ src/main/menu/projects-menu.js
@@ -4,6 +4,7 @@ const menu = {
   submenu: [
     {
       label: 'Manage Projects...',
+      accelerator: 'CmdOrCtrl+M',
       click: (menuItem, browserWindow) => browserWindow.send('IPC_SHOW_PROJECT_MANAGEMENT')
     }
   ]
