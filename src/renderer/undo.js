f371cc707534d0de9ec9e5700dfac3e3459bd93d
dehmer <horst.dehmer@syncpoint.io>
2020-03-30 17:13:10 +0200

    undo: limit undo/redo stacks to 32 commands each.

--- src/renderer/undo.js
+++ src/renderer/undo.js
@@ -1,5 +1,6 @@
 // TODO: reset buffers on project close/open
 
+const MAX_STACK = 32
 let undoStack = []
 let redoStack = []
 
@@ -9,6 +10,7 @@ export const undo = () => {
   head.apply()
   undoStack = tail
   redoStack.unshift(head.inverse())
+  redoStack.splice(MAX_STACK)
 }
 
 export const redo = () => {
@@ -17,10 +19,14 @@ export const redo = () => {
   head.apply()
   redoStack = tail
   undoStack.unshift(head.inverse())
+  undoStack.splice(MAX_STACK)
 }
 
 /**
  * NOTE: Command#apply() is expected to undo last change.
  * @param {object} command inversable command
  */
-export const push = command => undoStack.unshift(command)
+export const push = command => {
+  undoStack.unshift(command)
+  undoStack.splice(MAX_STACK)
+}
