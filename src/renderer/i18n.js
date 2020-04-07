7099c2e8074351919113d2a634889c0f8b8bdbbc
Thomas Halwax <thomas.halwax@syncpoint.io>
2020-04-07 20:34:05 +0200

    cleanup of i18n initialization

--- src/renderer/i18n.js
+++ src/renderer/i18n.js
@@ -1,6 +1,22 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 
-i18n.use(initReactI18next)
+const DEFAULT_I18N_NAMESPACE = 'web'
 
-export default i18n
+const addResourceBundle = i18nInfo => {
+  if (!i18n.hasResourceBundle(i18nInfo.lng, DEFAULT_I18N_NAMESPACE)) {
+    i18n.addResourceBundle(i18nInfo.lng, DEFAULT_I18N_NAMESPACE, i18nInfo.resourceBundle)
+  }
+}
+
+const applyLanguageSelection = i18nInfo => {
+  addResourceBundle(i18nInfo)
+  i18n.changeLanguage(i18nInfo.lng)
+}
+
+i18n.use(initReactI18next)
+i18n.init({ defaultNS: DEFAULT_I18N_NAMESPACE })
+
+export {
+  applyLanguageSelection
+}
