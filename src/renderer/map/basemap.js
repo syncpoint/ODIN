390a21328e68629b21404255160f72cd84d7f6c8
Thomas Halwax <thomas.halwax@syncpoint.io>
2020-04-09 11:09:07 +0200

    insert or replace basemap layer

--- src/renderer/map/basemap.js
+++ src/renderer/map/basemap.js
@@ -5,27 +5,51 @@ import { Tile as TileLayer } from 'ol/layer'
 import './epsg'
 import { defaultSource, from } from './sources'
 
-
+/* set by constructor */
 let olMap = null
 
-const use = source => {
-  /* switch basemap using source */
+const setBasemap = source => {
+
+  const baseLayer = new TileLayer({ source: source })
+  const layers = olMap.getLayers()
+
+  /*
+    We need to verify if the basemap layer is already
+    present and replace or insert it. Our naive approach is
+    to assume that the basemap is an instance of TileLayer
+    (vs VectorLayer). As long as we do not support VectorTileLayer
+    this is sufficient.
+
+    Due to the lifecycle of the map and the project
+    this may be done twice:
+      * at creation time (1)
+      * mount/unmout the map component (2)
+
+    TODO: Verify if this affects performance and we need to
+          add an additional check in order to skip this step
+          if the source has not changed.
+  */
+  const rootLayer = layers.item(0)
+  if (rootLayer && rootLayer instanceof TileLayer) {
+    return olMap.getLayers().setAt(0, baseLayer)
+  }
+  layers.insertAt(0, baseLayer)
 }
 
-const handleProjectLifecycle = async action => {
-  if (action !== 'open') return
-
+const fromPreferences = async () => {
   const source = project.preferences().basemap
   const tileSource = await (source ? from(source) : defaultSource())
+  setBasemap(tileSource)
+}
 
-  olMap.getLayers().insertAt(0, new TileLayer({ source: tileSource }))
+const handleProjectLifecycle = action => {
+  if (action !== 'open') return
+  fromPreferences()
 }
 
 project.register(handleProjectLifecycle)
 
 export default map => {
   olMap = map
-  return {
-    use
-  }
+  fromPreferences()
 }
