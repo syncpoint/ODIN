4a5c9f895e04d9128d5b38c5262d7a596555e010
Thomas Halwax <thomas.halwax@syncpoint.io>
2020-04-23 12:23:36 +0200

    using descriptors instead of OL sources

--- src/main/ipc/source-descriptors.js
+++ src/main/ipc/source-descriptors.js
@@ -6,13 +6,13 @@ const HOME = app.getPath('home')
 const ODIN_HOME = path.join(HOME, 'ODIN')
 const ODIN_SOURCES = path.join(ODIN_HOME, 'sources.json')
 
-const listSources = async () => {
+const listSourceDescriptors = async () => {
   if (!fs.existsSync(ODIN_SOURCES)) return []
 
   const sources = JSON.parse(await fs.promises.readFile(ODIN_SOURCES, 'utf-8'))
   return sources
 }
 
-ipcMain.handle('IPC_LIST_SOURCES', async () => {
-  return await listSources()
+ipcMain.handle('IPC_LIST_SOURCE_DESCRIPTORS', async () => {
+  return await listSourceDescriptors()
 })
