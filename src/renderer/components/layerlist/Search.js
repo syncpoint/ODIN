a9ddab5d3320cab84672cf1d0ee9c6171d0249ee
dehmer <horst.dehmer@syncpoint.io>
2020-05-11 09:51:08 +0200

    edit menu: reverted select-all handling (for now).

--- src/renderer/components/layerlist/Search.js
+++ src/renderer/components/layerlist/Search.js
@@ -1,4 +1,3 @@
-import { ipcRenderer } from 'electron'
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import { InputBase } from '@material-ui/core'
@@ -10,6 +9,8 @@ const useStyles = makeStyles((theme) => ({
     paddingTop: theme.spacing(1),
     paddingBottom: theme.spacing(1),
     fontSize: '120%',
+
+    // prevent placeholder from highlighting on select-all
     userSelect: 'none'
   }
 }))
@@ -17,33 +18,13 @@ const useStyles = makeStyles((theme) => ({
 export const Search = (/* props */) => {
   const classes = useStyles()
 
-  // Search: Prevent undo/redo when not focused.
-  const [readOnly, setReadOnly] = React.useState(false)
-  const ref = React.useRef()
-  const selectAll = React.useCallback(() => {
-    const input = ref.current
-    input.setSelectionRange(0, input.value.length)
-  }, [])
-
-  const handleBlur = () => {
-    ipcRenderer.removeListener('IPC_EDIT_SELECT_ALL', selectAll)
-    setReadOnly(true)
-  }
-
-  const handleFocus = () => {
-    ipcRenderer.on('IPC_EDIT_SELECT_ALL', selectAll)
-    setReadOnly(false)
-  }
+  // TODO: find way to prevent select-all highlighting when field has no focus
 
   return (
     <InputBase
       className={classes.search}
       placeholder={'Search...'}
       autoFocus
-      inputRef={ref}
-      readOnly={readOnly}
-      onFocus={handleFocus}
-      onBlur={handleBlur}
       disabled
     />
   )
