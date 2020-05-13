711ef7bd8aa7b888ecd148f534502d2f3c655945
dehmer <horst.dehmer@syncpoint.io>
2020-05-13 10:33:06 +0200

    layer actions: change enabled state  with selection.

--- src/renderer/components/layerlist/Actions.js
+++ src/renderer/components/layerlist/Actions.js
@@ -5,18 +5,19 @@ import Tooltip from '../Tooltip.js'
 import inputLayers from '../../project/input-layers'
 import selection from '../../selection'
 import URI from '../../project/URI'
+import { K, noop } from '../../../shared/combinators'
 
-const actions = [
+const actionDescriptors = [
   {
     icon: <LayersPlus/>,
     tooltip: 'Add Layer',
-    disabled: false,
+    disabled: true,
     action: () => inputLayers.createLayer()
   },
   {
     icon: <LayersMinus/>,
     tooltip: 'Delete Layer',
-    disabled: false,
+    disabled: true,
     action: () => {
       const selected = selection.selected(URI.isLayerId)
       if (!selected || !selected.length) return
@@ -26,16 +27,37 @@ const actions = [
   {
     icon: <ContentDuplicate/>,
     tooltip: 'Duplicate Layer',
-    disabled: true
+    disabled: true,
+    action: noop
   },
   {
     icon: <ExportVariant/>,
     tooltip: 'Share layer',
-    disabled: true
+    disabled: true,
+    action: noop
   }
 ]
 
+const reducer = (prev) => K([...prev])(next => {
+  const selected = selection.selected(URI.isLayerId)
+  next
+    .filter(action => action.action !== noop)
+    .forEach(action => (action.disabled = !selected.length))
+})
+
 export const Actions = (/* props */) => {
+  const [actions, dispatch] = React.useReducer(reducer, actionDescriptors)
+
+  React.useEffect(() => {
+    const handleEvent = () => dispatch()
+    selection.on('selected', handleEvent)
+    selection.on('deselected', handleEvent)
+
+    return () => {
+      selection.off('selected', handleEvent)
+      selection.off('deselected', handleEvent)
+    }
+  }, [])
 
   return actions.map(({ icon, tooltip, disabled, action }, index) => (
     <Tooltip key={index} title={tooltip}>
