a1d1b202662860f83dcbdc18b47402fd196f167b
Thomas Halwax <thomas.halwax@syncpoint.io>
2020-05-14 17:27:56 +0200

    error handling if projection definitions are missing

--- src/renderer/components/basemap/WMTSOptions.js
+++ src/renderer/components/basemap/WMTSOptions.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import { PropTypes } from 'prop-types'
-import { Card, CardContent, Typography } from '@material-ui/core'
+import { Card, CardContent, Typography, CircularProgress } from '@material-ui/core'
 
 import WMTSLayerTable from './WMTSLayerTable'
 import WMTSCapabilities from 'ol/format/WMTSCapabilities'
 import { get as getProjection } from 'ol/proj'
 
+import { useTranslation } from 'react-i18next'
+
 const WMTSOptions = props => {
   const { merge, onValidation } = props
+
+  const { t } = useTranslation()
 
   /* effects */
   const [capabilities, setCapabilities] = React.useState(null)
   const [selectedLayerId, setSelectedLayerId] = React.useState(props.options.layer)
   const [error, setError] = React.useState(null)
+  const [missingProjectionDefinitions, setMissingProjectionDefinitions] = React.useState([])
 
   React.useEffect(() => {
     const controller = new AbortController()
@@ -20,24 +25,41 @@ const WMTSOptions = props => {
 
     const fetchAndSetCapabilities = async () => {
       setError(null)
+      setCapabilities(null)
       try {
         const response = await fetch(props.options.url, { signal })
         if (!response.ok) { throw new Error(response.statusText) }
         const caps = (new WMTSCapabilities()).read(await response.text())
-        console.dir(caps)
         setCapabilities(caps)
       } catch (error) {
         setError(error.message)
       }
     }
     fetchAndSetCapabilities()
-
-    onValidation(!!props.options.layer)
+    onValidation(!!props.options.layer && capabilities && !error)
     return () => { controller.abort() }
   }, [])
 
+  // triggered by changes to the WMTS capabilities or by selecting a layer
+  React.useEffect(() => {
+    if (!capabilities || !selectedLayerId) return
+    const missingDefinitions = crs(capabilities, selectedLayerId)
+      .map(crs => ({
+        Identifier: crs.Identifier,
+        SupportedCRS: crs.SupportedCRS,
+        CRSCode: crs.SupportedCRS.replace(/urn:ogc:def:crs:(\w+):(.*:)?(\w+)$/, '$1:$3')
+      }))
+      .filter(crs => !getProjection(crs.CRSCode))
+
+    setMissingProjectionDefinitions(missingDefinitions)
+    onValidation(missingDefinitions.length === 0)
+  }, [capabilities, selectedLayerId])
+
   /* increases performance */
   const layers = React.useMemo(() => {
+    /*  Depending on the WMTS provider the abstract may be lengthy. In order
+        to avoid information overflow we cut off the abstract at a given length.
+    */
     const MAX_ABSTRACT_LENGTH = 140
 
     const getAbstract = layer => {
@@ -58,9 +80,7 @@ const WMTSOptions = props => {
   }, [capabilities])
 
   const wgs84BoundingBox = (wmtsCapabilites, layerId) => {
-    console.log(`extracting wgs84 bb for layer ${layerId}`)
     const layer = wmtsCapabilites.Contents.Layer.find(l => l.Identifier === layerId)
-    console.dir(layer)
     if (!layer) return null
     return layer.WGS84BoundingBox
   }
@@ -68,7 +88,7 @@ const WMTSOptions = props => {
   const crs = (caps, layerId) => {
     let providedCRS = []
     const layer = caps.Contents.Layer.find(l => l.Identifier === layerId)
-    if (!layer) return null
+    if (!layer) return providedCRS
     layer.TileMatrixSetLink.forEach(link => {
       providedCRS = providedCRS.concat(
         caps.Contents.TileMatrixSet
@@ -81,37 +101,31 @@ const WMTSOptions = props => {
 
   /* functions */
   const handleLayerSelected = layerId => {
+    setSelectedLayerId(layerId)
     if (layerId) {
-      setSelectedLayerId(layerId)
       merge({
         layer: layerId,
         wgs84BoundingBox: wgs84BoundingBox(capabilities, layerId)
       })
-      const missingCRS = crs(capabilities, layerId)
-        .map(crs => ({
-          Identifier: crs.Identifier,
-          SupportedCRS: crs.SupportedCRS,
-          CRSCode: crs.SupportedCRS.replace(/urn:ogc:def:crs:(\w+):(.*:)?(\w+)$/, '$1:$3')
-        }))
-        .filter(crs => {
-          const definition = getProjection(crs.CRSCode)
-          return definition ? null : crs
-        })
-      console.dir(missingCRS)
-      onValidation(missingCRS.length === 0)
-    } else {
-      onValidation(false)
     }
   }
 
   /* rendering */
-  if (!capabilities || error) {
+  if (error) {
     return (
       <Card variant="outlined">
         <CardContent>
           <Typography gutterBottom variant="body1" color="secondary">
-            Error: {error}
+            {t('basemapManagement.wmts.error')}: {error}
           </Typography>
+        </CardContent>
+      </Card>
+    )
+  } else if (!capabilities) {
+    return (
+      <Card variant="outlined">
+        <CardContent align="center">
+          <CircularProgress />
         </CardContent>
       </Card>
     )
@@ -137,6 +151,16 @@ const WMTSOptions = props => {
         selectedLayerIdentifier={selectedLayerId}
         onLayerSelected={handleLayerSelected}
       />
+      { missingProjectionDefinitions.length > 0
+        ? <Card variant="outlined" style={{ marginTop: '0.5em' }}>
+          <CardContent>
+            <Typography gutterBottom color="secondary">
+              {t('basemapManagement.wmts.missingProjections')}: {missingProjectionDefinitions.map(d => d.CRSCode).join(',')}
+            </Typography>
+          </CardContent>
+        </Card>
+        : null
+      }
     </>
   )
 }
