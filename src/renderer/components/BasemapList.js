d9b86de8ff0229555ae3fef4534437375dee98f5
Thomas Halwax <thomas.halwax@syncpoint.io>
2020-05-18 14:22:03 +0200

    DnD reordering of basemap source descriptors

--- src/renderer/components/BasemapList.js
+++ src/renderer/components/BasemapList.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
+import { DndProvider } from 'react-dnd'
+import Backend from 'react-dnd-html5-backend'
+import BasemapListItem from './basemap/BasemapListItem'
 
-import { ListItem, List, Paper, Typography } from '@material-ui/core'
+import { Paper } from '@material-ui/core'
 
 import { listSourceDescriptors } from '../map/basemap'
-import preferences from '../project/preferences'
 import { ipcRenderer } from 'electron'
 
 const useStyles = makeStyles(theme => ({
@@ -39,27 +41,38 @@ const BasemapList = props => {
     return () => ipcRenderer.removeListener('IPC_SOURCE_DESCRIPTORS_CHANGED', handleSourceDescriptorsChanged)
   }, [])
 
-  /*  The selcted descriptor is written to the project preferences */
-  const handleDescriptorSelected = descriptor => {
-    preferences.set('basemap', descriptor)
-  }
+  const moveBasemapItem = React.useCallback((sourceIndex, hoverIndex) => {
+    if (!sourceDescriptors) return
+    const item = sourceDescriptors[sourceIndex]
+    const shadow = [...sourceDescriptors]
+    shadow.splice(sourceIndex, 1)
+    shadow.splice(hoverIndex, 0, item)
+    setSourceDescriptors(shadow)
+  },
+  [sourceDescriptors])
 
   return (
-    <Paper className={classes.panel} elevation={6}>
-      <div className={classes.listContainer}>
-        <List>
-          {
-            sourceDescriptors.map(descriptor => (
-              <ListItem key={descriptor.id} button
-                onClick={event => handleDescriptorSelected(descriptor)}
-              >
-                <Typography variant="button">{descriptor.name}</Typography>
-              </ListItem>
-            ))
-          }
-        </List>
-      </div>
-    </Paper>
+
+    <DndProvider backend={Backend}>
+      <Paper className={classes.panel} elevation={6}>
+        <div className={classes.listContainer}>
+
+          <ul id="basemapItemList" style={{ listStyleType: 'none', padding: '4px' }}>
+            {
+              sourceDescriptors.map((descriptor, index) => (
+                <BasemapListItem
+                  key={descriptor.id}
+                  index={index}
+                  id={descriptor.id}
+                  text={descriptor.name}
+                  moveBasemapItem={moveBasemapItem}
+                />
+              ))
+            }
+          </ul>
+        </div>
+      </Paper>
+    </DndProvider>
   )
 }
 
