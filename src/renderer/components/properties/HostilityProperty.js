91333129490d6f710807ef08d0539c81e59f1ded
dehmer <horst.dehmer@syncpoint.io>
2020-05-21 08:58:56 +0200

    added some hostility states.

--- src/renderer/components/properties/HostilityProperty.js
+++ src/renderer/components/properties/HostilityProperty.js
@@ -21,11 +21,19 @@ const HostilityProperty = props => {
     >
       <MenuItem value={'*'}>N/A</MenuItem>
       <MenuItem value={'F'}>Friend</MenuItem>
+      <MenuItem value={'A'}>Assumed Friend</MenuItem>
       <MenuItem value={'H'}>Hostile</MenuItem>
       <MenuItem value={'N'}>Neutral</MenuItem>
       <MenuItem value={'U'}>Unknown</MenuItem>
       <MenuItem value={'J'}>Joker</MenuItem>
       <MenuItem value={'K'}>Faker</MenuItem>
+      <MenuItem value={'S'}>Suspect</MenuItem>
+      <MenuItem value={'P'}>Pending</MenuItem>
+      <MenuItem value={'G'}>Exercise Pending</MenuItem>
+      <MenuItem value={'W'}>Exercise Unknown</MenuItem>
+      <MenuItem value={'M'}>Exercise Assumed Friend</MenuItem>
+      <MenuItem value={'D'}>Exercise Friend</MenuItem>
+      <MenuItem value={'L'}>Exercise Neutral</MenuItem>
     </Select>
   )
 }
