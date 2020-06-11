75d45d19d188532f45d18345f8b1d04eea51a10a
dehmer <horst.dehmer@syncpoint.io>
2020-06-11 12:30:29 +0200

    #309: completed hostility status/color mapping.

--- src/renderer/map/style/default-style.js
+++ src/renderer/map/style/default-style.js
@@ -9,11 +9,27 @@ const lineDash = sidc => status(sidc) === 'A' ? [20, 10] : null
 const strokeColor = (sidc, n) => {
   const colorScheme = ColorSchemes.medium
   if (n === 'ENY') return colorScheme.red
+
+  // 2525C, table TABLE XIII:
   switch (identity(sidc)) {
-    case 'F': return colorScheme.blue
-    case 'H': return colorScheme.red
-    case 'N': return colorScheme.green
-    case 'U': return colorScheme.yellow
+    case 'A': // Assumed Friend
+    case 'F': // Friend
+    case 'M': // Exercise Assumed Friend
+    case 'D': // Exercise Friend
+      return colorScheme.blue
+    case 'H': // Hostile
+    case 'J': // Joker
+    case 'K': // Faker
+    case 'S': // Suspect
+      return colorScheme.red
+    case 'N': // Neutral
+    case 'L': // Exercise Neutral
+      return colorScheme.green
+    case 'U': // Unknown
+    case 'P': // Pending
+    case 'G': // Exercise Pending
+    case 'W': // Exercise Unknown
+      return colorScheme.yellow
     default: return 'black'
   }
 }
