ef54ea043095f42ecce6771467311fa439882282
Thomas Halwax <thomas.halwax@syncpoint.io>
2020-10-05 13:12:35 +0200

    improved emergency management props

--- src/renderer/components/properties/EEIProperties.js
+++ src/renderer/components/properties/EEIProperties.js
@@ -33,11 +33,12 @@ const EEIProperties = props => {
       <TextProperty label='Name' property='name' properties={props.properties} onCommit={props.update} className={classes.twoColumns}/>
       <TextProperty label='Unique Designation' property='t' properties={props.properties} onCommit={props.update}/>
       <TextProperty label='Quantity' property='c' properties={props.properties} onCommit={props.update}/>
+      <HostilityProperty properties={props.properties} onCommit={props.update} className={classes.twoColumns}/>
+      <TextProperty label='Date-Time Group' property='w' className={classes.twoColumns} properties={props.properties} onCommit={props.update}/>
       <TextProperty label='Speed' property='z' properties={props.properties} onCommit={props.update}/>
       <TextProperty label='Direction' property='q' properties={props.properties} onCommit={props.update}/>
       <TextProperty label='Staff Comments' property='g' className={classes.twoColumns} properties={props.properties} onCommit={props.update}/>
       <TextProperty label='Additional Information' property='h' className={classes.twoColumns} properties={props.properties} onCommit={props.update}/>
-      <HostilityProperty properties={props.properties} onCommit={props.update} className={classes.twoColumns}/>
     </Paper>
   )
 }
