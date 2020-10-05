14343f94c171269dc0748c670d4143babeb8a192
Thomas Halwax <thomas.halwax@syncpoint.io>
2020-10-05 12:30:15 +0200

    improved equipment props, see #181, #182, #184, #186, #187

--- src/renderer/components/properties/EquipmentProperties.js
+++ src/renderer/components/properties/EquipmentProperties.js
@@ -35,13 +35,14 @@ const EquipmentProperties = props => {
       <TextProperty label='Name' property='name' properties={props.properties} onCommit={props.update} className={classes.twoColumns}/>
       <TextProperty label='Unique Designation' property='t' properties={props.properties} onCommit={props.update}/>
       <TextProperty label='Quantity' property='c' properties={props.properties} onCommit={props.update}/>
+      <TextProperty label='Type' property='v' properties={props.properties} onCommit={props.update} />
+      <MobilityProperty properties={props.properties} onCommit={props.update}/>
+      <HostilityProperty properties={props.properties} onCommit={props.update} className={classes.twoColumns}/>
+      <TextProperty label='Date-Time Group' property='w' className={classes.twoColumns} properties={props.properties} onCommit={props.update}/>
       <TextProperty label='Speed' property='z' properties={props.properties} onCommit={props.update}/>
       <TextProperty label='Direction' property='q' properties={props.properties} onCommit={props.update}/>
-      <TextProperty label='Type' property='v' properties={props.properties} onCommit={props.update} className={classes.twoColumns} />
       <TextProperty label='Staff Comments' property='g' className={classes.twoColumns} properties={props.properties} onCommit={props.update}/>
       <TextProperty label='Additional Information' property='h' className={classes.twoColumns} properties={props.properties} onCommit={props.update}/>
-      <HostilityProperty properties={props.properties} onCommit={props.update}/>
-      <MobilityProperty properties={props.properties} onCommit={props.update}/>
       <StatusGroupFull properties={props.properties} onCommit={props.update}/>
     </Paper>
   )
