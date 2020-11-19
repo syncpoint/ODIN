b464ae5638cc9237dd2d871997b6304c29cd0865
Thomas Halwax <thomas.halwax@syncpoint.io>
2020-11-19 18:13:16 +0100

    user configurable min/max zoom values for XYZ tile provider
    closes #354

--- src/renderer/components/basemap/XYZOptions.js
+++ src/renderer/components/basemap/XYZOptions.js
@@ -1,17 +1,50 @@
 import React from 'react'
-import { Card, CardContent, Typography } from '@material-ui/core'
+import { PropTypes } from 'prop-types'
+import { Card, CardContent, Slider, Typography } from '@material-ui/core'
 
 import { useTranslation } from 'react-i18next'
 
 const XYZOptions = props => {
+
+  const MIN_ZOOM = 0
+  const MAX_ZOOM = 22
+
+  const { merge, options } = props
   const { t } = useTranslation()
+
+  const [zoomValues, setZoomValues] = React.useState([options.minZoom || MIN_ZOOM, options.maxZoom || MAX_ZOOM])
+
+  const handleChange = (_, newValue) => {
+    setZoomValues(newValue)
+    merge({
+      minZoom: newValue[0],
+      maxZoom: newValue[1]
+    })
+  }
+
   return (
     <Card variant="outlined">
       <CardContent>
-        <Typography>{t('basemapManagement.xyzNoOptions')}</Typography>
+        <Typography gutterBottom={true}>{t('basemapManagement.xyzZoomOptions')}</Typography>
+        <div style={{ marginTop: '3em' }}>
+          <Slider
+            value={zoomValues}
+            onChange={handleChange}
+            valueLabelDisplay="on"
+            aria-labelledby="range-slider"
+            marks={true}
+            min={MIN_ZOOM}
+            max={MAX_ZOOM}
+          />
+        </div>
       </CardContent>
     </Card>
   )
+}
+
+XYZOptions.propTypes = {
+  options: PropTypes.object,
+  merge: PropTypes.func
 }
 
 export default XYZOptions
