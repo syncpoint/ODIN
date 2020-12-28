d7224f1d3e3bf475ffcb15674aee19379dbb0c46
Thomas Halwax <thomas.halwax@syncpoint.io>
2020-12-28 11:54:06 +0100

    management of map attributions

--- src/renderer/components/basemap/Name.js
+++ src/renderer/components/basemap/Name.js
@@ -5,10 +5,11 @@ import { Card, CardContent, FormControl, Input, InputLabel } from '@material-ui/
 import { useTranslation } from 'react-i18next'
 
 const Name = props => {
-  const { classes } = props
+  const { classes, merge } = props
   const { t } = useTranslation()
 
   const [name, setName] = React.useState(props.name)
+  const [attributions, setAttributions] = React.useState(props.attributions)
   const [isValid, setIsValid] = React.useState(false)
 
   React.useEffect(() => {
@@ -23,15 +24,28 @@ const Name = props => {
     setName(event.target.value)
   }
 
+  const handleAttributionsChanged = (event) => {
+    setAttributions(event.target.value)
+  }
+
   return (
     <Card variant="outlined">
       <CardContent>
         <div id="editName">
           <FormControl error={!isValid} fullWidth className={classes.formControl}>
-            <InputLabel htmlFor="descriptorName">{t('basemapManagement.descriptorName')}</InputLabel>
+            <InputLabel htmlFor="name">{t('basemapManagement.descriptorName')}</InputLabel>
             <Input id="name" name="name" defaultValue={name} autoFocus={true}
               onChange={handlePropertyChanged}
               onBlur={() => props.onNameReady(name)}
+            />
+          </FormControl>
+        </div>
+        <div id="editAttribution">
+          <FormControl fullWidth className={classes.formControl}>
+            <InputLabel htmlFor="attributions">{t('basemapManagement.attributions')}</InputLabel>
+            <Input id="attributions" name="attributions" defaultValue={attributions}
+              onChange={handleAttributionsChanged}
+              onBlur={() => merge('attributions', attributions)}
             />
           </FormControl>
         </div>
@@ -41,7 +55,9 @@ const Name = props => {
 }
 Name.propTypes = {
   classes: PropTypes.object,
-  name: PropTypes.string.isRequired,
+  name: PropTypes.string,
+  attributions: PropTypes.string,
+  merge: PropTypes.func,
   onValidation: PropTypes.func,
   onNameReady: PropTypes.func
 }
