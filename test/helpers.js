e337ea2658658ae6dba9e7a0cf3c7e742595c5ee
dehmer <horst.dehmer@syncpoint.io>
2021-02-23 12:02:22 +0100

    updated webpack and dependencies.

--- test/helpers.js
+++
@@ -1,8 +0,0 @@
-import { mount, render, shallow, configure } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
-
-configure({ adapter: new Adapter() })
-
-global.mount = mount
-global.render = render
-global.shallow = shallow
