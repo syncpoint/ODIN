1304d3ed67380917ea572953dbd742d6ef028a26
dehmer <horst.dehmer@syncpoint.io>
2021-03-17 14:20:37 +0100

    #405: fixed.

--- src/main/bootstrap.js
+++ src/main/bootstrap.js
@@ -28,6 +28,8 @@ const RECENT_WINDOW_KEY = `${STATE_KEY}.recentWindow`
  */
 let appShallQuit = true
 
+let mainWindow
+
 /**
  * Merge current value (object) with supplied (map) function.
  */
@@ -50,79 +52,87 @@ const sendi18Info = (receiver, lng) => {
   receiver.webContents.send('IPC_LANGUAGE_CHANGED', { lng: lng, resourceBundle: i18n.getResourceBundle(lng, 'web') })
 }
 
+
+/**
+ *
+ */
+const createWindow = async (projectOptions) => {
+  const devServer = process.argv.indexOf('--noDevServer') === -1
+  const hotDeployment = process.defaultApp ||
+    /[\\/]electron-prebuilt[\\/]/.test(process.execPath) ||
+    /[\\/]electron[\\/]/.test(process.execPath)
+
+  const windowUrl = (hotDeployment && devServer)
+    ? new URL('http://localhost:8080/index.html')
+    : new URL(`file:${path.join(app.getAppPath(), 'dist', 'index.html')}`)
+
+  const title = await windowTitle(projectOptions.path)
+
+  mainWindow = new BrowserWindow({
+    ...projectOptions,
+    title: title,
+    show: false,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true
+    }
+  })
+
+  /** the path property is required to identify the project */
+  mainWindow.path = projectOptions.path
+
+  /* remember the window settings per project, so the key is the project folder name (which is a UUID) */
+  const id = projectId(projectOptions.path)
+  const key = windowKey(id)
+  merge(key)(props => ({ ...props, ...projectOptions }), {})
+
+  const updateBounds = () => merge(key)(props => ({ ...props, ...mainWindow.getBounds() }))
+  mainWindow.on('page-title-updated', event => event.preventDefault())
+  mainWindow.on('move', updateBounds)
+  mainWindow.on('resize', updateBounds)
+
+  // TODO: support fullscreen
+
+  /* restore the existing viewport if exists or maximize the window */
+  if (projectOptions.viewport) {
+    mainWindow.viewport = projectOptions.viewport
+  } else {
+    mainWindow.maximize()
+  }
+
+  /* send app setting for i18n to window */
+  const languageChangedHandler = lng => {
+    sendi18Info(mainWindow, lng)
+  }
+  i18n.on('languageChanged', languageChangedHandler)
+  mainWindow.once('close', () => {
+    mainWindow = null
+    i18n.off('languageChanged', languageChangedHandler)
+  })
+
+  /* (re)establish electron's normal "quit the app if no more windows are open" behavior */
+  appShallQuit = true
+
+  /* allow users to find their recently used projects access by data/time */
+  projects.mergeMetadata(projectOptions.path, { lastAccess: new Date() })
+
+  /*  Remember this window/project to be the most recent.
+        We will use this key to identify the recent project and
+        use it on startup ('app-ready') and when we switch projects ('IPC_SWITCH_PROJECT').
+    */
+  merge(RECENT_WINDOW_KEY)(() => projectOptions.path)
+  await mainWindow.loadURL(windowUrl.href)
+  mainWindow.show()
+}
+
+
 /**
  * create the project window.
  *
  * @param {*} options window options
  */
 const createProjectWindow = async (options) => {
-
-  const createWindow = async (projectOptions) => {
-    const devServer = process.argv.indexOf('--noDevServer') === -1
-    const hotDeployment = process.defaultApp ||
-      /[\\/]electron-prebuilt[\\/]/.test(process.execPath) ||
-      /[\\/]electron[\\/]/.test(process.execPath)
-
-    const windowUrl = (hotDeployment && devServer)
-      ? new URL('http://localhost:8080/index.html')
-      : new URL(`file:${path.join(app.getAppPath(), 'dist', 'index.html')}`)
-
-    const title = await windowTitle(projectOptions.path)
-
-    const window = new BrowserWindow({
-      ...projectOptions,
-      title: title,
-      show: false,
-      webPreferences: {
-        nodeIntegration: true,
-        enableRemoteModule: true
-      }
-    })
-
-    /** the path property is required to identify the project */
-    window.path = projectOptions.path
-
-    /* remember the window settings per project, so the key is the project folder name (which is a UUID) */
-    const id = projectId(projectOptions.path)
-    const key = windowKey(id)
-    merge(key)(props => ({ ...props, ...projectOptions }), {})
-
-    const updateBounds = () => merge(key)(props => ({ ...props, ...window.getBounds() }))
-    window.on('page-title-updated', event => event.preventDefault())
-    window.on('move', updateBounds)
-    window.on('resize', updateBounds)
-    // TODO: support fullscreen
-
-    /* restore the existing viewport if exists or maximize the window */
-    if (projectOptions.viewport) {
-      window.viewport = projectOptions.viewport
-    } else {
-      window.maximize()
-    }
-
-    /* send app setting for i18n to window */
-    const languageChangedHandler = lng => {
-      sendi18Info(window, lng)
-    }
-    i18n.on('languageChanged', languageChangedHandler)
-    window.on('close', () => {
-      i18n.off('languageChanged', languageChangedHandler)
-    })
-
-    /* (re)establish electron's normal "quit the app if no more windows are open" behavior */
-    appShallQuit = true
-    /* allow users to find their recently used projects access by data/time */
-    projects.mergeMetadata(projectOptions.path, { lastAccess: new Date() })
-
-    /*  Remember this window/project to be the most recent.
-          We will use this key to identify the recent project and
-          use it on startup ('app-ready') and when we switch projects ('IPC_SWITCH_PROJECT').
-      */
-    merge(RECENT_WINDOW_KEY)(() => projectOptions.path)
-    await window.loadURL(windowUrl.href)
-    window.show()
-  }
-
   if (options && options.path && projects.exists(options.path)) {
     return createWindow(options)
   }
@@ -136,74 +146,111 @@ const createProjectWindow = async (options) => {
 }
 
 /**
+ * Handle event app/ready.
+ */
+const ready = () => {
+
+  /*
+    Setting the appId is required to allow desktop notifications on the Windows platform.
+    Please make sure this value is THE SAME as in 'electron-builder.yml'.
+  */
+  app.setAppUserModelId('io.syncpoint.odin')
+
+  /*
+    Checking for updates is always executed but will only do it's job if the user has enabled
+    the autoUpdate option.
+  */
+  autoUpdate.checkForUpdates()
+
+  /* try to restore persisted window state */
+  const state = Object.values(settings.get(WINDOWS_KEY, {}))
+
+  if (!state || state.length === 0) {
+    return createProjectWindow(/* will create a new untiteled project */)
+  }
+
+  const mostRecentProject = settings.get(RECENT_WINDOW_KEY)
+  const recentProject = state.find(setting => setting.path === mostRecentProject)
+
+  if (!recentProject) return createProjectWindow(/* will create a new untiteled project */)
+  createProjectWindow(recentProject)
+}
+
+const windowAllClosed = () => {
+  if (!appShallQuit) return
+  autoUpdate.cancel()
+  app.quit()
+}
+
+const secondInstance = (/* event, commandLine, workingDirectory */) => {
+  // Someone tried to run a second instance, we should focus our window.
+  if (mainWindow) {
+    if (mainWindow.isMinimized()) mainWindow.restore()
+    mainWindow.focus()
+  } else {
+    createProjectWindow()
+  }
+}
+
+const webContentsCreated = (_, contents) => {
+  contents.on('new-window', handleNavigationEvent)
+  contents.on('will-navigate', handleNavigationEvent)
+  contents.on('will-redirect', handleNavigationEvent)
+}
+
+const ipcViewportChanged = (event, viewport) => {
+  const id = projectId(event.sender.getOwnerBrowserWindow().path)
+  merge(windowKey(id))(props => ({ ...props, viewport }), {})
+}
+
+const ipcSwitchProject = async (event, projectPath) => {
+  const sender = event.sender.getOwnerBrowserWindow()
+  if (sender.path === projectPath) return
+
+  /*
+    prevent electron from quitting the application
+    this will be restored to TRUE in the createProjectWindow function
+  */
+  appShallQuit = false
+
+  // Wait for window to be closed, before creating a new one.
+  // Else mainWindow reference would be overwritten since
+  // creating new windows kicks in before old window is closed.
+
+  await new Promise((resolve) => {
+    sender.once('close', resolve)
+    sender.close()
+  })
+
+  /*
+    restore the window settings
+    if no settings exist we create a default window
+  */
+  const id = projectId(projectPath)
+  const persistedSettings = settings.get(windowKey(id), { path: projectPath })
+  createProjectWindow(persistedSettings)
+}
+
+const ipcAppRenderingCompleted = event => {
+  const sender = event.sender.getOwnerBrowserWindow()
+  sendi18Info(sender, i18n.language)
+}
+
+/**
  * registers all app listeners and loads either the last project used or creates a new one
  */
 const bootstrap = () => {
-
-  app.on('ready', () => {
-    /*
-      Setting the appId is required to allow desktop notifications on the Windows platform.
-      Please make sure this value is THE SAME as in 'electron-builder.yml'.
-    */
-    app.setAppUserModelId('io.syncpoint.odin')
-
-    /*
-      Checking for updates is always executed but will only do it's job if the user has enabled
-      the autoUpdate option.
-    */
-    autoUpdate.checkForUpdates()
-
-    /* try to restore persisted window state */
-    const state = Object.values(settings.get(WINDOWS_KEY, {}))
-
-    if (!state || state.length === 0) {
-      return createProjectWindow(/* will create a new untiteled project */)
-    }
-
-    const mostRecentProject = settings.get(RECENT_WINDOW_KEY)
-    const recentProject = state.find(setting => setting.path === mostRecentProject)
-
-    if (!recentProject) return createProjectWindow(/* will create a new untiteled project */)
-    createProjectWindow(recentProject)
-
-  })
-
-  app.on('window-all-closed', () => {
-    if (!appShallQuit) return
-    autoUpdate.cancel()
-    app.quit()
-  })
+  app.on('ready', ready)
+  app.on('window-all-closed', windowAllClosed)
+  app.on('second-instance', secondInstance)
 
   /* security */
-  app.on('web-contents-created', (_, contents) => {
-    contents.on('new-window', handleNavigationEvent)
-    contents.on('will-navigate', handleNavigationEvent)
-    contents.on('will-redirect', handleNavigationEvent)
-  })
+  app.on('web-contents-created', webContentsCreated)
 
-  ipcMain.on('IPC_VIEWPORT_CHANGED', (event, viewport) => {
-    const id = projectId(event.sender.getOwnerBrowserWindow().path)
-    merge(windowKey(id))(props => ({ ...props, viewport }), {})
-  })
+  ipcMain.on('IPC_VIEWPORT_CHANGED', ipcViewportChanged)
 
   /* emitted by the renderer process in order to change projects */
-  ipcMain.on('IPC_SWITCH_PROJECT', (event, projectPath) => {
-    const sender = event.sender.getOwnerBrowserWindow()
-    if (sender.path === projectPath) return
-    /*
-      prevent electron from quitting the application
-      this will be restored to TRUE in the createProjectWindow function
-    */
-    appShallQuit = false
-    sender.close()
-    /*
-      restore the window settings
-      if no settings exist we create a default window
-    */
-    const id = projectId(projectPath)
-    const persistedSettings = settings.get(windowKey(id), { path: projectPath })
-    createProjectWindow(persistedSettings)
-  })
+  ipcMain.on('IPC_SWITCH_PROJECT', ipcSwitchProject)
 
   /*
     Emitted by the renderer process in order to save a preview
@@ -221,11 +268,7 @@ const bootstrap = () => {
   ipcMain.on('IPC_SHARE_PNG', viewAsPng)
 
   /* emitted by renderer/App */
-  ipcMain.on('IPC_APP_RENDERING_COMPLETED', event => {
-    const sender = event.sender.getOwnerBrowserWindow()
-    sendi18Info(sender, i18n.language)
-  })
-
+  ipcMain.on('IPC_APP_RENDERING_COMPLETED', ipcAppRenderingCompleted)
 }
 
 export default bootstrap
--- src/main/main.js
+++ src/main/main.js
@@ -1,6 +1,4 @@
-/* eslint-disable dot-notation */
-
-import { Menu, ipcMain } from 'electron'
+import { Menu, ipcMain, app } from 'electron'
 import { buildFromTemplate } from '../main/menu/menu'
 import settings from 'electron-settings'
 import bootstrap from './bootstrap'
@@ -8,38 +6,40 @@ import i18n, { languageKey } from '../i18n'
 import config from '../shared/config'
 import './ipc/clipboard'
 
+
 /*  As of version 8.2 electron does not support adding/removing menu items dynamically.
     In order to add/remove recently used projects we need to rebuild and
     set the application menu.
 */
 
-const buildApplicationMenu = () => {
-  const menu = buildFromTemplate(i18n)
+const setApplicationMenu = (args = {}) => {
+  const menu = buildFromTemplate(i18n, args)
   Menu.setApplicationMenu(menu)
 }
 
-i18n.on('initialized', () => {
+const languageChanged = lng => {
+  settings.set(languageKey, lng)
+  setApplicationMenu()
+}
+
+const initialized = () => {
+
   /*
     By using an environment variable or a ".env" file users may change the language used.
     The ".env" file must be placed in the working directory and is NOT part of the
     software distribution.
   */
 
-  i18n.on('languageChanged', lng => {
-    settings.set(languageKey, lng)
-    buildApplicationMenu()
-  })
-
+  i18n.on('languageChanged', languageChanged)
   i18n.changeLanguage(config.language || settings.get(languageKey, 'en'))
   bootstrap()
-})
+}
 
-ipcMain.on('IPC_GRID_TOGGLED', (event, type) => {
-  const menu = buildFromTemplate(i18n, { grid: type })
-  Menu.setApplicationMenu(menu)
-})
 
-ipcMain.on('IPC_LABELS_TOGGLED', (event, show) => {
-  const menu = buildFromTemplate(i18n, { label: show })
-  Menu.setApplicationMenu(menu)
-})
+// Prevent more than once process instance from running.
+if (!app.requestSingleInstanceLock()) app.quit()
+else {
+  i18n.on('initialized', initialized)
+  ipcMain.on('IPC_GRID_TOGGLED', (_, type) => setApplicationMenu({ grid: type }))
+  ipcMain.on('IPC_LABELS_TOGGLED', (_, show) => setApplicationMenu({ label: show }))
+}
