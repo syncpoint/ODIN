b84a67fb9e7a59d3e70cf6389c77cf017246bfac
Thomas Halwax <thomas.halwax@syncpoint.io>
2021-03-22 09:58:45 +0100

    wrapped DeleteIcon with am IconButton

--- src/renderer/components/references/ReferenceList.js
+++ src/renderer/components/references/ReferenceList.js
@@ -1,21 +1,23 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Link, List, ListItem, ListItemSecondaryAction } from '@material-ui/core'
+import { IconButton, Link, List, ListItem, ListItemSecondaryAction } from '@material-ui/core'
 import DeleteIcon from '@material-ui/icons/DeleteForeverOutlined'
 
 const ReferenceList = props => {
   const { references = [], onDelete = () => {} } = props
 
   return (
-    <List>
+    <List >
       {references.sort((l, r) => {
         if (l.name === r.name) return 0
         return (l.name < r.name ? -1 : 1)
       }).map(
-        reference => <ListItem key={reference.id} >
+        reference => <ListItem key={reference.id} disableGutters >
           <Link variant='body2' href={reference.url} style={{ overflow: 'hidden' }}>{reference.name}</Link>
           <ListItemSecondaryAction>
-            <DeleteIcon onClick={() => onDelete(reference.url)}/>
+            <IconButton edge='end' onClick={() => onDelete(reference.url)}>
+              <DeleteIcon />
+            </IconButton>
           </ListItemSecondaryAction>
         </ListItem>
       )}
