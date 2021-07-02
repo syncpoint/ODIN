acf23d510e2d006ceed504c50f862b748ca92b90
Thomas Halwax <thomas.halwax@syncpoint.io>
2021-07-02 18:30:14 +0200

    added language specific url for user manual

--- src/main/menu/help-menu.js
+++ src/main/menu/help-menu.js
@@ -6,7 +6,8 @@ const menu = i18n => (
         label: i18n.t('help.onlineUserManual'),
         click: async () => {
           const { shell } = require('electron')
-          await shell.openExternal('https://syncpoint.github.io/odin-manual')
+          const languagePostfix = i18n.language === 'en' ? 'index.en' : ''
+          await shell.openExternal(`https://www.syncpoint.io/odin-manual/${languagePostfix}`)
         }
       }
     ]
