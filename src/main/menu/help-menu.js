0f5996a7604043978ae8eb7d1b836f84ad5e80b5
Thomas Halwax <thomas.halwax@syncpoint.io>
2021-07-30 14:41:37 +0200

    added e-mail and slack channel to the help menu

--- src/main/menu/help-menu.js
+++ src/main/menu/help-menu.js
@@ -1,3 +1,5 @@
+import { shell } from 'electron'
+
 const menu = i18n => (
   {
     label: i18n.t('help.name'),
@@ -5,9 +7,20 @@ const menu = i18n => (
       {
         label: i18n.t('help.onlineUserManual'),
         click: async () => {
-          const { shell } = require('electron')
           const languagePostfix = i18n.language === 'en' ? 'index.en' : ''
           await shell.openExternal(`https://syncpoint.github.io/odin-manual/${languagePostfix}`)
+        }
+      },
+      {
+        label: i18n.t('help.onlineSupportSlack'),
+        click: async () => {
+          await shell.openExternal('https://join.slack.com/t/odin-c2is/shared_invite/zt-ttboh2l0-0r92XQC___tTQEG1Ku1spA')
+        }
+      },
+      {
+        label: i18n.t('help.onlineSupportEMail'),
+        click: async () => {
+          await shell.openExternal('mailto:odin-support@syncpoint.io?subject=ODIN support')
         }
       }
     ]
