0509d233172d89c74117d09f5b0b9d5adc3c3848
Harry WIPPERFÜRTH <harry wipperfuerth@syncpoint.io>
2022-02-16 09:43:51 +0100

    new UI french

--- src/main/menu/language-menu.js
+++ src/main/menu/language-menu.js
@@ -1,7 +1,7 @@
 const languageMenu = i18n => {
   return {
     label: i18n.t('Language'),
-    submenu: ['de', 'en'].map((languageCode) => {
+    submenu: ['de', 'en', 'fr'].map((languageCode) => {
       return {
         label: i18n.t(languageCode),
         type: 'radio',
