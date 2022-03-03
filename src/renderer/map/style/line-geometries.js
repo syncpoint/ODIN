7233ed7bb7582ec15af68048c9ceb87aaa62f6ea
Harry WIPPERFÜRTH <harry wipperfuerth@syncpoint.io>
2022-03-03 15:57:25 +0100

    new task provide physical security

--- src/renderer/map/style/line-geometries.js
+++ src/renderer/map/style/line-geometries.js
@@ -3,6 +3,7 @@ import * as style from 'ol/style'
 import * as TS from '../ts'
 import echelons from './echelons'
 import * as fences from './fences'
+import { biggerFont } from './default-style'
 
 const linearTarget = ({ styles, line }) => {
   const coords = TS.coordinates(line)
@@ -1050,5 +1051,52 @@ geometries['G*T*VAE---'] = ({ styles, line }) => {
       TS.lineString(R.props([3, 4, 5], xs))
     ])),
     styles.dashedLine(TS.lineString(R.props([0, 1, 2], xs)))
+  ]
+}
+
+/**
+ * TACGRP.TSK.PPS
+ * TASKS / PROVIDE PHYSICAL SECURITY (AUT ONLY)
+ */
+geometries['G*T*VAP---'] = ({ styles, resolution, line: lineString }) => {
+  const coords = TS.coordinates(lineString)
+  const segment = TS.segment(coords)
+  const angle = segment.angle()
+  const center = segment.midPoint()
+  const radius = segment.getLength() / 2
+  const length = segment.getLength()
+  const deg2rad = Math.PI / 180
+
+  const xs = R.range(0, 7)
+    .map(i => Math.PI / 16 * i + angle)
+    .map(angle => TS.projectCoordinate(center)([angle, radius]))
+
+  const xt = R.range(10, 17)
+    .map(i => Math.PI / 16 * i + angle)
+    .map(angle => TS.projectCoordinate(center)([angle, radius]))
+
+  const xv = TS.projectCoordinates(length, angle, coords[0])([
+    [0.1, -0.1], [0, 0], [-0.1, -0.1],
+    [0.9, -0.1], [1, 0], [1.1, -0.1]
+  ])
+
+  const xw = TS.projectCoordinates(length, angle, coords[0])([
+    [0.5, -0.5]
+  ])
+
+  return [
+    styles.solidLine(TS.collect([
+      TS.lineString(xs),
+      TS.lineString(xt),
+      TS.lineString(R.props([0, 1, 2], xv)),
+      TS.lineString(R.props([3, 4, 5], xv))
+    ])),
+    styles.text(TS.point(xw[0]), {
+      font: biggerFont(),
+      flip: true,
+      textAlign: () => 'center',
+      rotation: Math.PI - angle + 360 / 2 * deg2rad,
+      text: 'PSEC'
+    })
   ]
 }
