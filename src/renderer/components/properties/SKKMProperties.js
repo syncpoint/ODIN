97637abbb31e593601e1e7e566c9f43cb99ff482
Thomas Halwax <thomas.halwax@syncpoint.io>
2022-04-03 13:23:02 +0200

    added property panel for SKKM

---
+++ src/renderer/components/properties/SKKMProperties.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { makeStyles } from '@material-ui/core/styles'
+
+import TextProperty from './TextProperty'
+
+const useStyles = makeStyles((/* theme */) => ({
+  twoColumns: { gridColumn: '1 / span 2' }
+}))
+
+const SKKMDangerProperties = props => {
+  const classes = useStyles()
+  const properties = props.getProperties()
+
+  return (
+    <>
+      <TextProperty label='Name' property='name' properties={properties} onCommit={props.update} className={classes.twoColumns}/>
+      <TextProperty label='Unique Designation' property='t' properties={properties} onCommit={props.update} className={classes.twoColumns}/>
+      <TextProperty label='Date-Time Group' property='w' className={classes.twoColumns} properties={properties} onCommit={props.update}/>
+      <TextProperty label='Speed' property='z' properties={properties} onCommit={props.update}/>
+      <TextProperty label='Direction' property='q' properties={properties} onCommit={props.update}/>
+      <TextProperty label='Additional Information' property='h' className={classes.twoColumns} properties={properties} onCommit={props.update}/>
+    </>
+  )
+}
+
+SKKMDangerProperties.propTypes = {
+  getProperties: PropTypes.func.isRequired,
+  update: PropTypes.func.isRequired
+}
+
+export default SKKMDangerProperties
